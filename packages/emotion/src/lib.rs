d6bb3d97c4dd36bc09fbaa7db279d6d82fd1dbb1
강동윤 <kdy1997.dev@gmail.com>
2021-09-19 17:57:53 +0900

    Collect imports

--- packages/emotion/src/lib.rs
+++ packages/emotion/src/lib.rs
@@ -1,3 +1,5 @@
+use swc_ecma_ast::*;
+use swc_ecma_utils::{ident::IdentLike, Id};
 use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut};
 
 pub fn emotion_plugin(config: Config) -> impl VisitMut + Fold {
@@ -11,7 +13,12 @@ pub fn emotion_plugin(config: Config) -> impl VisitMut + Fold {
 pub struct Config {}
 
 #[derive(Debug, Default)]
-struct State {}
+struct State {
+    /// Imports from `@emotion/css`.
+    ///
+    /// e.g. `import { css } from "@emotion/css";`
+    vanilla_css: Vec<Id>,
+}
 
 struct Emotion {
     #[allow(dead_code)]
@@ -22,4 +29,41 @@ struct Emotion {
 impl VisitMut for Emotion {
     // Reduce binary size.
     noop_visit_mut_type!();
+
+    fn visit_mut_import_decl(&mut self, i: &mut ImportDecl) {
+        match &*i.src.value {
+            "@emotion/css" => {
+                self.state
+                    .vanilla_css
+                    .extend(find_import(&i.specifiers, "css"));
+            }
+            _ => {}
+        }
+    }
+}
+
+fn find_import(ss: &[ImportSpecifier], wanted: &str) -> Vec<Id> {
+    let mut ids = vec![];
+
+    for s in ss {
+        match s {
+            ImportSpecifier::Named(n) => match &n.imported {
+                Some(imported) => {
+                    if imported.sym == *wanted {
+                        ids.push(n.local.to_id());
+                    }
+                }
+                None => {
+                    if n.local.sym == *wanted {
+                        ids.push(n.local.to_id());
+                    }
+                }
+            },
+            _ => {
+                todo!("default import and namespacaed import for emotion plugins")
+            }
+        }
+    }
+
+    ids
 }
