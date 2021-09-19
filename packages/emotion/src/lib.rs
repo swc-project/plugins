e75180253f1b0cab2b2b2bf858f6ec272bb5028b
강동윤 <kdy1997.dev@gmail.com>
2021-09-19 17:52:22 +0900

    fields

--- packages/emotion/src/lib.rs
+++ packages/emotion/src/lib.rs
@@ -1,10 +1,23 @@
 use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut};
 
-pub fn emotion_plugin() -> impl VisitMut + Fold {
-    as_folder(Emotion {})
+pub fn emotion_plugin(config: Config) -> impl VisitMut + Fold {
+    as_folder(Emotion {
+        config,
+        state: Default::default(),
+    })
 }
 
-struct Emotion {}
+#[derive(Debug)]
+pub struct Config {}
+
+#[derive(Debug, Default)]
+struct State {}
+
+struct Emotion {
+    #[allow(dead_code)]
+    config: Config,
+    state: State,
+}
 
 impl VisitMut for Emotion {
     // Reduce binary size.
--- packages/emotion/tests/fixture.rs
+++ packages/emotion/tests/fixture.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 use swc_common::chain;
 use swc_ecma_transforms_base::resolver::resolver;
-use swc_emotion::emotion_plugin;
+use swc_emotion::{emotion_plugin, Config};
 
 #[testing::fixture("tests/fixtures/**/input.js")]
 fn fixture(input: PathBuf) {
@@ -9,7 +9,7 @@ fn fixture(input: PathBuf) {
 
     swc_ecma_transforms_testing::test_fixture(
         Default::default(),
-        &|_t| chain!(resolver(), emotion_plugin()),
+        &|_t| chain!(resolver(), emotion_plugin(Config {})),
         &input,
         &output,
     );
