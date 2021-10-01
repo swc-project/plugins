c3a64000395572d8e606f7f4a8f4677852c4cb23
Donny <kdy1997.dev@gmail.com>
2021-10-02 00:24:13 +0900

    Base

--- packages/jest/src/lib.rs
+++ packages/jest/src/lib.rs
@@ -1,22 +1,7 @@
-/// The function which exports the root module of the library.
-///
-/// The root module is exported inside a static of `LibHeader` type,
-/// which has this extra metadata:
-///
-/// - The abi_stable version number used by the dynamic library.
-///
-/// - A constant describing the layout of the exported root module,and every
-///   type it references.
-///
-/// - A lazily initialized reference to the root module.
-///
-/// - The constructor function of the root module.
+use abi_stable::{export_root_module, prefix_type::PrefixTypeTrait};
+use swc_plugin::{SwcPlugin, SwcPluginRef};
+
 #[export_root_module]
-pub fn get_library() -> SwcPlugin_Ref {
-    ExampleLib {
-        new_appender,
-        new_boxed_interface,
-        append_string,
-    }
-    .leak_into_prefix()
+pub fn get_library() -> SwcPluginRef {
+    SwcPlugin {}.leak_into_prefix()
 }
