062675d208d251ce6303a0e6165ce544b5b022f5
Donny <kdy1997.dev@gmail.com>
2021-10-09 15:10:10 +0900

    jest plugin

--- packages/jest/src/lib.rs
+++ packages/jest/src/lib.rs
@@ -1,7 +1,97 @@
 use abi_stable::{export_root_module, prefix_type::PrefixTypeTrait};
+use phf::phf_set;
+use swc_common::util::take::Take;
+use swc_ecmascript::{
+    ast::*,
+    utils::{prepend_stmts, StmtLike},
+    visit::{as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith},
+};
 use swc_plugin::{SwcPlugin, SwcPluginRef};
 
 #[export_root_module]
 pub fn get_library() -> SwcPluginRef {
     SwcPlugin {}.leak_into_prefix()
+}
+
+static HOIST_METHODS: phf::Set<&str> = phf_set![
+    "mock",
+    "unmock",
+    "enableAutomock",
+    "disableAutomock",
+    "deepUnmock"
+];
+
+pub fn jest() -> impl Fold + VisitMut {
+    as_folder(Jest)
+}
+
+struct Jest;
+
+impl Jest {
+    fn visit_mut_stmt_like<T>(&mut self, orig: &mut Vec<T>)
+    where
+        T: StmtLike + VisitMutWith<Self>,
+    {
+        for item in &mut *orig {
+            item.visit_mut_with(self);
+        }
+
+        let items = orig.take();
+
+        let mut new = Vec::with_capacity(items.len());
+        let mut hoisted = Vec::with_capacity(8);
+        items.into_iter().for_each(|item| {
+            match item.try_into_stmt() {
+                Ok(stmt) => match &stmt {
+                    Stmt::Expr(ExprStmt { expr, .. }) => match &**expr {
+                        Expr::Call(CallExpr {
+                            callee: ExprOrSuper::Expr(callee),
+                            ..
+                        }) => match &**callee {
+                            Expr::Member(
+                                callee
+                                @
+                                MemberExpr {
+                                    computed: false, ..
+                                },
+                            ) => match &callee.obj {
+                                ExprOrSuper::Super(_) => new.push(T::from_stmt(stmt)),
+                                ExprOrSuper::Expr(callee_obj) => match &**callee_obj {
+                                    Expr::Ident(i) if i.sym == *"jest" => match &*callee.prop {
+                                        Expr::Ident(prop) if HOIST_METHODS.contains(&*prop.sym) => {
+                                            hoisted.push(T::from_stmt(stmt));
+                                            return;
+                                        }
+                                        _ => new.push(T::from_stmt(stmt)),
+                                    },
+                                    _ => new.push(T::from_stmt(stmt)),
+                                },
+                            },
+                            _ => new.push(T::from_stmt(stmt)),
+                        },
+                        _ => new.push(T::from_stmt(stmt)),
+                    },
+
+                    _ => new.push(T::from_stmt(stmt)),
+                },
+                Err(node) => new.push(node),
+            };
+        });
+
+        prepend_stmts(&mut new, hoisted.into_iter());
+
+        *orig = new;
+    }
+}
+
+impl VisitMut for Jest {
+    noop_visit_mut_type!();
+
+    fn visit_mut_stmts(&mut self, stmts: &mut Vec<Stmt>) {
+        self.visit_mut_stmt_like(stmts)
+    }
+
+    fn visit_mut_module_items(&mut self, items: &mut Vec<ModuleItem>) {
+        self.visit_mut_stmt_like(items)
+    }
 }
