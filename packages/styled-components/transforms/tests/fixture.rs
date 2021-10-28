399caf3379d04caaf2fc22eb047ddda3df7b5005
Donny <kdy1997.dev@gmail.com>
2021-10-28 17:49:43 +0900

    Test: jsx syntax

--- packages/styled-components/transforms/tests/fixture.rs
+++ packages/styled-components/transforms/tests/fixture.rs
@@ -1,15 +1,23 @@
+#![deny(unused)]
+
 use std::path::PathBuf;
 use styled_components::styled_components;
 use swc_common::chain;
 use swc_ecma_transforms_testing::test_fixture;
-use swc_ecmascript::transforms::resolver;
+use swc_ecmascript::{
+    parser::{EsConfig, Syntax},
+    transforms::resolver,
+};
 
 #[testing::fixture("tests/fixtures/**/code.js")]
 fn fixture(input: PathBuf) {
     let dir = input.parent().unwrap();
 
     test_fixture(
-        Default::default(),
+        Syntax::Es(EsConfig {
+            jsx: true,
+            ..Default::default()
+        }),
         &|_| chain!(resolver(), styled_components()),
         &input,
         &dir.join("output.js"),
