b0067de9843b0b0748251d093f83d45b233269fc
Donny <kdy1997.dev@gmail.com>
2021-10-28 17:42:44 +0900

    Test

---
+++ packages/styled-components/transforms/tests/fixture.rs
@@ -0,0 +1,17 @@
+use std::path::PathBuf;
+use styled_components::styled_components;
+use swc_common::chain;
+use swc_ecma_transforms_testing::test_fixture;
+use swc_ecmascript::transforms::resolver;
+
+#[testing::fixture("tests/fixtures/**/code.js")]
+fn fixture(input: PathBuf) {
+    let dir = input.parent().unwrap();
+
+    test_fixture(
+        Default::default(),
+        &|_| chain!(resolver(), styled_components()),
+        &input,
+        &dir.join("output.js"),
+    )
+}
