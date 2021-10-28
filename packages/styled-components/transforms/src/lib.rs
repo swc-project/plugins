efc04a25452abbb8413087335aebbd1ef8c2d408
Donny <kdy1997.dev@gmail.com>
2021-10-28 16:59:01 +0900

    WIP

--- packages/styled-components/transforms/src/lib.rs
+++ packages/styled-components/transforms/src/lib.rs
@@ -1,7 +1,4 @@
-use swc_ecmascript::{
-    ast::JSXAttr,
-    visit::{as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith},
-};
+use swc_ecmascript::visit::{Fold, VisitMut};
 use visitors::template_literals::transpile::transpile_css_prop;
 
 mod css;
@@ -9,17 +6,5 @@ mod utils;
 mod visitors;
 
 pub fn styled_components() -> impl Fold + VisitMut {
-    as_folder(StyledComponents)
-}
-
-struct StyledComponents;
-
-impl VisitMut for StyledComponents {
-    noop_visit_mut_type!();
-
-    fn visit_mut_jsx_attr(&mut self, n: &mut JSXAttr) {
-        n.visit_mut_children_with(self);
-
-        n.visit_mut_with(&mut transpile_css_prop());
-    }
+    transpile_css_prop()
 }
--- packages/styled-components/transforms/src/visitors/template_literals/transpile.rs
+++ packages/styled-components/transforms/src/visitors/template_literals/transpile.rs
@@ -1,6 +1,9 @@
 //! Port of https://github.com/styled-components/babel-plugin-styled-components/blob/a20c3033508677695953e7a434de4746168eeb4e/src/visitors/transpileCssProp.js
 
-use swc_ecmascript::visit::{as_folder, noop_visit_mut_type, Fold, VisitMut};
+use swc_ecmascript::{
+    ast::*,
+    visit::{as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith},
+};
 
 pub fn transpile_css_prop() -> impl Fold + VisitMut {
     as_folder(TranspileCssProp)
@@ -10,4 +13,24 @@ struct TranspileCssProp;
 
 impl VisitMut for TranspileCssProp {
     noop_visit_mut_type!();
+
+    fn visit_mut_module(&mut self, n: &mut Module) {
+        // TODO: Skip if there are no css prop usage
+        n.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_script(&mut self, n: &mut Script) {
+        // TODO: Skip if there are no css prop usage
+        n.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_jsx_attr(&mut self, n: &mut JSXAttr) {
+        n.visit_mut_children_with(self);
+
+        if n.name != "css" {
+            return;
+        }
+
+        n.visit_mut_with(&mut transpile_css_prop());
+    }
 }
