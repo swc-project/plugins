a7a3de8749969ed9379ec49ff0cae2e330e4bdb3
Donny <kdy1997.dev@gmail.com>
2021-11-02 00:05:00 +0900

    Fix

--- scripts/ci/install-swc-dev.sh
+++ scripts/ci/install-swc-dev.sh
@@ -7,5 +7,10 @@ echo "Downloading swc-dev@${LATEST_TAG}"
 
 BIN_URL="https://github.com/swc-project/swc-dev/releases/download/$LATEST_TAG/swc-dev-$RUNNER_OS"
 
-echo "Using `$BIN_URL`"
+echo "Using $BIN_URL"
+
+mkdir -p swc-dev-built
+curl $BIN_URL -o swc-dev-built/swc-dev
+echo "$(pwd)/swc-dev-built" >> $GITHUB_PATH
+
 echo $LATEST_TAG
