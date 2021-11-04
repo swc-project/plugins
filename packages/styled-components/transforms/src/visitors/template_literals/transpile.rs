f0ee9e0ad4bfa535abf11ced2860bf04e627cedb
Donny <kdy1997.dev@gmail.com>
2021-11-04 17:49:24 +0900

    Some work

--- packages/styled-components/transforms/src/visitors/template_literals/transpile.rs
+++ packages/styled-components/transforms/src/visitors/template_literals/transpile.rs
@@ -1,6 +1,7 @@
 //! Port of https://github.com/styled-components/babel-plugin-styled-components/blob/a20c3033508677695953e7a434de4746168eeb4e/src/visitors/transpileCssProp.js
 
-use swc_common::DUMMY_SP;
+use swc_atoms::JsWord;
+use swc_common::{Spanned, DUMMY_SP};
 use swc_ecmascript::{
     ast::*,
     utils::{prepend, private_ident},
@@ -46,18 +47,61 @@ impl VisitMut for TranspileCssProp {
         }
     }
 
-    fn visit_mut_jsx_attr(&mut self, n: &mut JSXAttr) {
+    fn visit_mut_jsx_element(&mut self, n: &mut JSXElement) {
         n.visit_mut_children_with(self);
 
-        if !matches!(&n.name,JSXAttrName::Ident(i) if &*i.sym == "css") {
-            return;
+        for attr in &n.opening.attrs {
+            match &*attr {
+                JSXAttrOrSpread::JSXAttr(attr) => {
+                    if !matches!(&attr.name, JSXAttrName::Ident(i) if &*i.sym == "css") {
+                        continue;
+                    }
+
+                    let import_name = self
+                        .import_name
+                        .get_or_insert_with(|| private_ident!("styled"))
+                        .clone();
+
+                    let mut name = get_name(&n.opening.name);
+                    name = get_first_letter_uppercased(&name);
+
+                    let id: Ident =
+                        private_ident!(n.opening.name.span(), format!("Styled{}", name));
+                }
+                JSXAttrOrSpread::SpreadElement(_) => {}
+            }
         }
+    }
+}
 
-        let import_name = self
-            .import_name
-            .get_or_insert_with(|| private_ident!("styled"))
-            .clone();
+fn get_first_letter_uppercased(s: &str) -> String {
+    let mut c = s.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+    }
+}
 
-        n.visit_mut_with(&mut transpile_css_prop());
+fn get_name(el: &JSXElementName) -> String {
+    match el {
+        JSXElementName::Ident(v) => v.sym.to_string(),
+        JSXElementName::JSXMemberExpr(e) => {
+            format!("{}.{}", get_name_of_jsx_obj(&e.obj), e.prop.sym).into()
+        }
+        _ => {
+            unimplemented!("get_name for namespaced jsx element")
+        }
+    }
+}
+
+fn get_name_of_jsx_obj(el: &JSXObject) -> JsWord {
+    match el {
+        JSXObject::Ident(v) => v.sym.clone(),
+        JSXObject::JSXMemberExpr(e) => {
+            format!("{}.{}", get_name_of_jsx_obj(&e.obj), e.prop.sym).into()
+        }
+        _ => {
+            unimplemented!("get_name for namespaced jsx element")
+        }
     }
 }
