843fb69f89bd93461fd03008b98f39c865d4ddbe
Donny <kdy1997.dev@gmail.com>
2021-11-05 16:25:18 +0900

    Move some functions to util

--- packages/styled-components/transforms/src/utils/mod.rs
+++ packages/styled-components/transforms/src/utils/mod.rs
@@ -1,1 +1,24 @@
+use std::borrow::Cow;
 
+use swc_ecmascript::ast::*;
+
+pub(crate) fn get_prop_key_as_expr(p: &Prop) -> Cow<Expr> {
+    match p {
+        Prop::Shorthand(p) => Cow::Owned(Expr::Ident(p.clone())),
+        Prop::KeyValue(p) => prop_name_to_expr(&p.key),
+        Prop::Assign(p) => Cow::Owned(Expr::Ident(p.key.clone())),
+        Prop::Getter(p) => prop_name_to_expr(&p.key),
+        Prop::Setter(p) => prop_name_to_expr(&p.key),
+        Prop::Method(p) => prop_name_to_expr(&p.key),
+    }
+}
+
+pub(crate) fn prop_name_to_expr(p: &PropName) -> Cow<Expr> {
+    match p {
+        PropName::Ident(p) => Cow::Owned(Expr::Ident(p.clone())),
+        PropName::Str(p) => Cow::Owned(Expr::Lit(Lit::Str(p.clone()))),
+        PropName::Num(p) => Cow::Owned(Expr::Lit(Lit::Num(p.clone()))),
+        PropName::BigInt(p) => Cow::Owned(Expr::Lit(Lit::BigInt(p.clone()))),
+        PropName::Computed(e) => Cow::Borrowed(&e.expr),
+    }
+}
--- packages/styled-components/transforms/src/visitors/template_literals/transpile.rs
+++ packages/styled-components/transforms/src/visitors/template_literals/transpile.rs
@@ -12,6 +12,8 @@ use swc_ecmascript::{
     visit::{as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith},
 };
 
+use crate::utils::get_prop_key_as_expr;
+
 static TAG_NAME_REGEX: Lazy<Regex> =
     Lazy::new(|| Regex::new("^[a-z][a-z\\d]*(\\-[a-z][a-z\\d]*)?$").unwrap());
 
@@ -524,27 +526,6 @@ fn get_prop_name(p: &Prop) -> Option<&PropName> {
         Prop::Getter(p) => Some(&p.key),
         Prop::Setter(p) => Some(&p.key),
         Prop::Method(p) => Some(&p.key),
-    }
-}
-
-fn get_prop_key_as_expr(p: &Prop) -> Cow<Expr> {
-    match p {
-        Prop::Shorthand(p) => Cow::Owned(Expr::Ident(p.clone())),
-        Prop::KeyValue(p) => prop_name_to_expr(&p.key),
-        Prop::Assign(p) => Cow::Owned(Expr::Ident(p.key.clone())),
-        Prop::Getter(p) => prop_name_to_expr(&p.key),
-        Prop::Setter(p) => prop_name_to_expr(&p.key),
-        Prop::Method(p) => prop_name_to_expr(&p.key),
-    }
-}
-
-fn prop_name_to_expr(p: &PropName) -> Cow<Expr> {
-    match p {
-        PropName::Ident(p) => Cow::Owned(Expr::Ident(p.clone())),
-        PropName::Str(p) => Cow::Owned(Expr::Lit(Lit::Str(p.clone()))),
-        PropName::Num(p) => Cow::Owned(Expr::Lit(Lit::Num(p.clone()))),
-        PropName::BigInt(p) => Cow::Owned(Expr::Lit(Lit::BigInt(p.clone()))),
-        PropName::Computed(e) => Cow::Borrowed(&e.expr),
     }
 }
 
