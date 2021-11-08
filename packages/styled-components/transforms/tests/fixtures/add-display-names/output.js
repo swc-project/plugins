412d93c30afd0a052e7f48bed727b3fe90df1e76
Donny <kdy1997.dev@gmail.com>
2021-11-08 16:13:50 +0900

    Update test refs

--- packages/styled-components/transforms/tests/fixtures/add-display-names/output.js
+++ packages/styled-components/transforms/tests/fixtures/add-display-names/output.js
@@ -1,33 +1,34 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 const Test = styled.div.withConfig({
-  displayName: "Test"
-})`width:100%;`;
-const Test2 = styled('div').withConfig({
-  displayName: "Test2"
+    displayName: "Test"
+})`
+  width: 100%;
+`;
+const Test2 = styled("div").withConfig({
+    displayName: "Test2"
 })``;
 const Test3 = true ? styled.div.withConfig({
-  displayName: "Test3"
+    displayName: "Test3"
 })`` : styled.div.withConfig({
-  displayName: "Test3"
+    displayName: "Test3"
 })``;
 const styles = {
-  One: styled.div.withConfig({
-    displayName: "One"
-  })``
+    One: styled.div.withConfig({
+        displayName: "One"
+    })``
 };
 let Component;
 Component = styled.div.withConfig({
-  displayName: "Component"
+    displayName: "Component"
 })``;
 const WrappedComponent = styled(Inner).withConfig({
-  displayName: "WrappedComponent"
+    displayName: "WrappedComponent"
 })``;
-
-class ClassComponent {}
-
-ClassComponent.Child = styled.div.withConfig({
-  displayName: "Child"
-})``;
+class ClassComponent {
+    static Child = styled.div.withConfig({
+        displayName: "Child"
+    })``;
+}
 var GoodName = BadName = styled.div.withConfig({
-  displayName: "GoodName"
+    displayName: "GoodName"
 })``;
