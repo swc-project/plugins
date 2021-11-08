fe87c9895c0040e8981454ebe146fdbaaa1d2e6e
Donny <kdy1997.dev@gmail.com>
2021-11-08 15:57:05 +0900

    Update test refs

--- packages/styled-components/transforms/tests/fixtures/add-identifier/output.js
+++ packages/styled-components/transforms/tests/fixtures/add-identifier/output.js
@@ -1,21 +1,23 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 const Test = styled.div.withConfig({
-  componentId: "sc-1289dod-0"
-})`width:100%;`;
+    componentId: "sc-278245034064319926907629316816044298872-0"
+})`
+  width: 100%;
+`;
 const Test2 = true ? styled.div.withConfig({
-  componentId: "sc-1289dod-1"
+    componentId: "sc-278245034064319926907629316816044298872-1"
 })`` : styled.div.withConfig({
-  componentId: "sc-1289dod-2"
+    componentId: "sc-278245034064319926907629316816044298872-2"
 })``;
 const styles = {
-  One: styled.div.withConfig({
-    componentId: "sc-1289dod-3"
-  })``
+    One: styled.div.withConfig({
+        componentId: "sc-278245034064319926907629316816044298872-3"
+    })``
 };
 let Component;
 Component = styled.div.withConfig({
-  componentId: "sc-1289dod-4"
+    componentId: "sc-278245034064319926907629316816044298872-4"
 })``;
 const WrappedComponent = styled(Inner).withConfig({
-  componentId: "sc-1289dod-5"
+    componentId: "sc-278245034064319926907629316816044298872-5"
 })``;
