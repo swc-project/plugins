e3dff4ef1c6c13bbb6592ba5dd48e8d7b28653ff
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-23 17:41:52 +0900

    impl script

--- scripts/update-bump-all.sh
+++ scripts/update-bump-all.sh
@@ -5,6 +5,19 @@ function up {
     cargo upgrade --workspace $1
 }
 
+function bump {
+    cargo mono bump $1 --breaking
+}
+
 up swc_common
+up tesitng
 up swc_ecmascript
+up swc_ecma_transforms_testing
 up swc_plugin
+
+
+bump jest
+bump styled_components
+
+# Ensure that Cargo.lock is up-to-date
+cargo metadata --offline --format-version 1 > /dev/null
