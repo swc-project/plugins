21c08a43b59fe4a23709e6f9a41cf74282d8cae1
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-30 23:36:12 +0900

    chore(styled-components): Publish `styled-components` to npm (#24)

--- scripts/update-bump-all.sh
+++ scripts/update-bump-all.sh
@@ -9,15 +9,22 @@ function bump {
     cargo mono bump $1 --breaking
 }
 
+function bumpNpm {
+    (cd ./packages/$1 && yarn version)
+}
+
+up swc_atoms
 up swc_common
-up tesitng
+up testing
 up swc_ecmascript
 up swc_ecma_transforms_testing
 up swc_plugin
 
-
 bump jest
 bump styled_components
+
+bumpNpm jest
+bumpNpm styled-components
 
 # Ensure that Cargo.lock is up-to-date
 cargo metadata --offline --format-version 1 > /dev/null
