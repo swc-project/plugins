af44f9e4c3da417330fe11604f60595e94d7c3fd
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-30 23:13:16 +0900

    Patch script

--- scripts/update-bump-all.sh
+++ scripts/update-bump-all.sh
@@ -9,15 +9,21 @@ function bump {
     cargo mono bump $1 --breaking
 }
 
+function bumpNpm {
+    (cd $1 && yarn version)
+}
+
 up swc_common
 up tesitng
 up swc_ecmascript
 up swc_ecma_transforms_testing
 up swc_plugin
 
-
 bump jest
 bump styled_components
+
+bumpNpm jest
+bumpNpm styled-components
 
 # Ensure that Cargo.lock is up-to-date
 cargo metadata --offline --format-version 1 > /dev/null
