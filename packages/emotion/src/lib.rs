9e9cf547632574854b3142f3106606cbab5d80c8
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-31 00:15:11 +0900

    Okay...

--- packages/emotion/src/lib.rs
+++ packages/emotion/src/lib.rs
@@ -1,8 +1,2 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+#[path = "../../../vendor/next.js/packages/next-swc/crates/core/src/emotion/mod.rs"]
+mod imp;
