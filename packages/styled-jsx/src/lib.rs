7661822b2f866b5fb3afbe6be17f8702fe0cf20b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-31 00:24:10 +0900

    Almost

--- packages/styled-jsx/src/lib.rs
+++ packages/styled-jsx/src/lib.rs
@@ -6,10 +6,10 @@ use swc_plugin::plugin_transform;
 
 #[plugin_transform]
 fn emotion(program: Program, _plugin_config: String, _: String) -> Program {
-    let program = program.fold_with(&mut imp::emotion());
+    let program = program.fold_with(&mut imp::styled_jsx());
 
     program
 }
 
-#[path = "../../../vendor/next.js/packages/next-swc/crates/core/src/emotion/mod.rs"]
+#[path = "../../../vendor/next.js/packages/next-swc/crates/core/src/styled_jsx/mod.rs"]
 mod imp;
