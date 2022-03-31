e934b00b122231cc6f693338d5c789198ed92bfc
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-31 23:21:23 +0900

    Fix

---
+++ packages/transform-imports/README.md
@@ -0,0 +1,17 @@
+# transform-imports
+
+## Config
+
+```json
+[
+  "transform-imports",
+  {
+    "react-bootstrap": {
+      "transform": "react-bootstrap/lib/{{member}}"
+    },
+    "lodash": {
+      "transform": "lodash/{{member}}"
+    }
+  }
+]
+```
--- packages/transform-imports/src/lib.rs
+++ packages/transform-imports/src/lib.rs
@@ -3,7 +3,8 @@ use swc_plugin::{plugin_transform, TransformPluginProgramMetadata};
 
 #[plugin_transform]
 fn transform_imports(program: Program, data: TransformPluginProgramMetadata) -> Program {
-    let program = program.fold_with(&mut imp::modularize_imports(imp::Config {}));
+    let packages = serde_json::from_str(&data.plugin_config).expect("invalid packages");
+    let program = program.fold_with(&mut imp::modularize_imports(imp::Config { packages }));
 
     program
 }
