f4603738e450fae936d6721078b751bf0f2f3214
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-07 15:30:40 +0900

    Fix

--- packages/styled-jsx/src/lib.rs
+++ packages/styled-jsx/src/lib.rs
@@ -3,14 +3,11 @@ use swc_ecmascript::{ast::Program, visit::FoldWith};
 use swc_plugin::{plugin_transform, TransformPluginProgramMetadata};
 
 #[plugin_transform]
-fn styled_jsx(program: Program, _: TransformPluginProgramMetadata) -> Program {
+fn styled_jsx_plugin(program: Program, _: TransformPluginProgramMetadata) -> Program {
     // TODO(kdy1): This is wrong, but it does not use cm
     let cm = Lrc::new(SourceMap::default());
 
-    let program = program.fold_with(&mut imp::styled_jsx(cm, FileName::Anon));
+    let program = program.fold_with(&mut styled_jsx::styled_jsx(cm, FileName::Anon));
 
     program
 }
-
-#[path = "../../../vendor/next.js/packages/next-swc/crates/styled_jsx/src/lib.rs"]
-mod imp;
