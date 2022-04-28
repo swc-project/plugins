b0a17f5454db21fbb62acc018c689478d3b85387
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-28 12:35:38 +0900

    chore(deps): Update swc (#31)

--- packages/styled-components/src/lib.rs
+++ packages/styled-components/src/lib.rs
@@ -1,9 +1,9 @@
-use std::{cell::RefCell, rc::Rc};
-
-use styled_components::{analyzer, transpile_css_prop, Config, State};
+use serde::Deserialize;
+use styled_components::Config;
+use swc_common::FileName;
 use swc_plugin::{
     ast::{Program, VisitMutWith},
-    chain, plugin_transform, TransformPluginProgramMetadata,
+    plugin_transform, TransformPluginProgramMetadata,
 };
 
 #[plugin_transform]
@@ -11,15 +11,24 @@ fn styled_components(mut program: Program, data: TransformPluginProgramMetadata)
     let config = serde_json::from_str::<Config>(&data.plugin_config)
         .expect("invalid config for styled-components");
 
-    let state: Rc<RefCell<State>> = Default::default();
-    let config = Rc::new(config);
+    let ctx = serde_json::from_str::<SwcContext>(&data.transform_context).expect("invalid context");
+    let file_name = match ctx.filename {
+        Some(s) => FileName::Real(s.into()),
+        None => FileName::Anon,
+    };
 
-    let mut pass = chain!(
-        analyzer(config.clone(), state.clone()),
-        transpile_css_prop()
-    );
+    // TODO: Use correct value
+    let src_file_hash = 0;
+
+    let mut pass = styled_components::styled_components(file_name, src_file_hash, config);
 
     program.visit_mut_with(&mut pass);
 
     program
+}
+
+#[derive(Debug, Deserialize)]
+struct SwcContext {
+    #[serde(default)]
+    filename: Option<String>,
 }
