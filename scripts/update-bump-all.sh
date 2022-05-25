c8855a02be0538e60e5a23f52d9cddb7c7ef0bd7
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-25 11:36:15 +0900

    chore(deps): Update swc (#39)

--- scripts/update-bump-all.sh
+++ scripts/update-bump-all.sh
@@ -2,7 +2,7 @@
 set -eu
 
 function up {
-    cargo upgrade --workspace $1
+    cargo upgrade --workspace $@
 }
 
 function bump {
@@ -17,17 +17,9 @@ function bumpNpm {
 # Delete tags
 git tag -d $(git tag -l)
 
-up swc_atoms
-up swc_common
-up testing
-up swc_ecmascript
-up swc_ecma_transforms_testing
-up swc_plugin
+up swc_atoms swc_common testing swc_ecmascript swc_ecma_transforms_testing swc_plugin
 
-up styled_jsx
-up styled_components
-up modularize_imports
-up swc_emotion
+up styled_jsx styled_components modularize_imports swc_emotion
 
 bump swc_plugin_jest
 bump swc_plugin_styled_jsx
