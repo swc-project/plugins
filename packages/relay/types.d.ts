899f841f2aba38c81059733b6ff89f299a638768
XiNiHa <me@xiniha.dev>
2022-06-17 21:15:56 +0900

    feat(relay): add esm support

--- packages/relay/types.d.ts
+++ packages/relay/types.d.ts
@@ -2,6 +2,7 @@ declare module "@swc/plugin-relay" {
   export interface Config {
     rootDir: string;
     artifactDirectory?: string;
+    module: "commonjs" | "esm";
     language: "typescript" | "flow";
   }
 }
