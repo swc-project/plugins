9b02a07f514ce65b61aefc983ff84729673bc03c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-20 18:23:45 +0900

    Copy

---
+++ packages/loadable-components/src/lib.rs
@@ -0,0 +1,20 @@
+use swc_core::{
+    ast::Program,
+    plugin::{plugin_transform, proxies::TransformPluginProgramMetadata},
+    visit::FoldWith,
+};
+
+#[plugin_transform]
+fn transform_imports_plugin(program: Program, data: TransformPluginProgramMetadata) -> Program {
+    let packages = serde_json::from_str(
+        &data
+            .get_transform_plugin_config()
+            .expect("failed to get plugin config for transform-imports"),
+    )
+    .expect("invalid packages");
+    let program = program.fold_with(&mut modularize_imports::modularize_imports(
+        modularize_imports::Config { packages },
+    ));
+
+    program
+}
