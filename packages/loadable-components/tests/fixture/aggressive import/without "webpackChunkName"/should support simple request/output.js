55f5fde229af2fb191b382afb28b9c3803328174
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-21 08:48:34 +0900

    Update test refs

--- packages/loadable-components/tests/fixture/aggressive import/without "webpackChunkName"/should support simple request/output.js
+++ packages/loadable-components/tests/fixture/aggressive import/without "webpackChunkName"/should support simple request/output.js
@@ -1,9 +1,9 @@
 loadable({
     resolved: {},
-    chunkName(props) {
+    chunkName (props) {
         return `${props.foo}`.replace(/[^a-zA-Z0-9_!§$()=\\-^°]+/g, "-");
     },
-    isReady(props) {
+    isReady (props) {
         const key = this.resolve(props);
         if (this.resolved[key] !== true) {
             return false;
@@ -13,25 +13,23 @@ loadable({
         }
         return false;
     },
-    importAsync: props => import(
-        /* webpackChunkName: "[request]" */
-        `./${props.foo}`),
-    requireAsync(props) {
+    importAsync: (props)=>import(/*webpackChunkName: "[request]"*/ `./${props.foo}`),
+    requireAsync (props) {
         const key = this.resolve(props);
         this.resolved[key] = false;
-        return this.importAsync(props).then(resolved => {
+        return this.importAsync(props).then((resolved)=>{
             this.resolved[key] = true;
             return resolved;
         });
     },
-    requireSync(props) {
+    requireSync (props) {
         const id = this.resolve(props);
         if (typeof __webpack_require__ !== 'undefined') {
             return __webpack_require__(id);
         }
         return eval('module.require')(id);
     },
-    resolve(props) {
+    resolve (props) {
         if (require.resolveWeak) {
             return require.resolveWeak(`./${props.foo}`);
         }
