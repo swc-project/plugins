cebde3f58e97925ec03b000e5b90ffd751547ef2
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-20 18:50:42 +0900

    WIP

--- packages/loadable-components/src/lib.rs
+++ packages/loadable-components/src/lib.rs
@@ -1,7 +1,19 @@
 use swc_core::{
     ast::Program,
     plugin::{plugin_transform, proxies::TransformPluginProgramMetadata},
+    visit::{VisitMut, VisitMutWith},
 };
 
 #[plugin_transform]
-fn loadable_components_plugin(program: Program, data: TransformPluginProgramMetadata) -> Program {}
+fn loadable_components_plugin(
+    mut program: Program,
+    data: TransformPluginProgramMetadata,
+) -> Program {
+    program.visit_mut_with(&mut Loadable {});
+
+    program
+}
+
+struct Loadable {}
+
+impl VisitMut for Loadable {}
