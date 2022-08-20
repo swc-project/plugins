25ef086342c798916e00e9fc91ea3816cf6dcd07
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-21 08:58:55 +0900

    Update test refs

--- packages/loadable-components/tests/fixture/aggressive import/with "webpackChunkName"/should replace it/output.js
+++ packages/loadable-components/tests/fixture/aggressive import/with "webpackChunkName"/should replace it/output.js
@@ -1,53 +1,38 @@
 loadable({
     resolved: {},
-
-    chunkName(props) {
-        return `\${props.foo}`.replace(/[^a-zA-Z0-9_!§$()=\\-^°]+/g, "-");
+    chunkName (props) {
+        return `${props.foo}`.replace(/[^a-zA-Z0-9_!§$()=\\-^°]+/g, "-");
     },
-
-    isReady(props) {
+    isReady (props) {
         const key = this.resolve(props);
-
         if (this.resolved[key] !== true) {
             return false;
         }
-
         if (typeof __webpack_modules__ !== 'undefined') {
             return !!__webpack_modules__[key];
         }
-
         return false;
     },
-
-    importAsync: props => import(
-        /* webpackChunkName: "[request]" */
-        `./\${props.foo}`),
-
-    requireAsync(props) {
+    importAsync: (props)=>import(/*webpackChunkName: "[request]"*/ `./${props.foo}`),
+    requireAsync (props) {
         const key = this.resolve(props);
         this.resolved[key] = false;
-        return this.importAsync(props).then(resolved => {
+        return this.importAsync(props).then((resolved)=>{
             this.resolved[key] = true;
             return resolved;
         });
     },
-
-    requireSync(props) {
+    requireSync (props) {
         const id = this.resolve(props);
-
         if (typeof __webpack_require__ !== 'undefined') {
             return __webpack_require__(id);
         }
-
         return eval('module.require')(id);
     },
-
-    resolve(props) {
+    resolve (props) {
         if (require.resolveWeak) {
-            return require.resolveWeak(`./\${props.foo}`);
+            return require.resolveWeak(`./${props.foo}`);
         }
-
-        return eval('require.resolve')(`./\${props.foo}`);
+        return eval('require.resolve')(`./${props.foo}`);
     }
-
 });
