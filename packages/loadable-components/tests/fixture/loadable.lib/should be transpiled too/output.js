9d9d27a228e6719dc037e3cd8771ba8a187b799f
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-21 08:40:49 +0900

    Update test refs

--- packages/loadable-components/tests/fixture/loadable.lib/should be transpiled too/output.js
+++ packages/loadable-components/tests/fixture/loadable.lib/should be transpiled too/output.js
@@ -1,53 +1,38 @@
 loadable.lib({
     resolved: {},
-
-    chunkName() {
+    chunkName () {
         return "moment";
     },
-
-    isReady(props) {
+    isReady (props) {
         const key = this.resolve(props);
-
         if (this.resolved[key] !== true) {
             return false;
         }
-
         if (typeof __webpack_modules__ !== 'undefined') {
             return !!__webpack_modules__[key];
         }
-
         return false;
     },
-
-    importAsync: () => import(
-        /* webpackChunkName: "moment" */
-        'moment'),
-
-    requireAsync(props) {
+    importAsync: ()=>import(/*webpackChunkName: "moment"*/ 'moment'),
+    requireAsync (props) {
         const key = this.resolve(props);
         this.resolved[key] = false;
-        return this.importAsync(props).then(resolved => {
+        return this.importAsync(props).then((resolved)=>{
             this.resolved[key] = true;
             return resolved;
         });
     },
-
-    requireSync(props) {
+    requireSync (props) {
         const id = this.resolve(props);
-
         if (typeof __webpack_require__ !== 'undefined') {
             return __webpack_require__(id);
         }
-
         return eval('module.require')(id);
     },
-
-    resolve() {
+    resolve () {
         if (require.resolveWeak) {
-            return require.resolveWeak("moment");
+            return require.resolveWeak('moment');
         }
-
-        return eval('require.resolve')("moment");
+        return eval('require.resolve')('moment');
     }
-
 });
