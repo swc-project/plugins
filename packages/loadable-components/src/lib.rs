cf5a02cb061ffb4660638dce7c24c19dc2eec44d
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-20 18:26:05 +0900

    Adjust

--- packages/loadable-components/src/lib.rs
+++ packages/loadable-components/src/lib.rs
@@ -5,16 +5,4 @@ use swc_core::{
 };
 
 #[plugin_transform]
-fn transform_imports_plugin(program: Program, data: TransformPluginProgramMetadata) -> Program {
-    let packages = serde_json::from_str(
-        &data
-            .get_transform_plugin_config()
-            .expect("failed to get plugin config for transform-imports"),
-    )
-    .expect("invalid packages");
-    let program = program.fold_with(&mut modularize_imports::modularize_imports(
-        modularize_imports::Config { packages },
-    ));
-
-    program
-}
+fn loadable_components_plugin(program: Program, data: TransformPluginProgramMetadata) -> Program {}
