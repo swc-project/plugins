776bf8c20f94356a46715ab92e5c8ef6458f97bd
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-21 09:35:56 +0900

    Update test refs

--- packages/loadable-components/tests/fixture/aggressive import/without "webpackChunkName"/should support complex request/output.js
+++ packages/loadable-components/tests/fixture/aggressive import/without "webpackChunkName"/should support complex request/output.js
@@ -1,53 +1,38 @@
 loadable({
     resolved: {},
-
-    chunkName(props) {
+    chunkName (props) {
         return `dir-${props.foo}-test`.replace(/[^a-zA-Z0-9_!§$()=\\-^°]+/g, "-");
     },
-
-    isReady(props) {
+    isReady (props) {
         const key = this.resolve(props);
-
         if (this.resolved[key] !== true) {
             return false;
         }
-
         if (typeof __webpack_modules__ !== 'undefined') {
             return !!__webpack_modules__[key];
         }
-
         return false;
     },
-
-    importAsync: props => import(
-        /* webpackChunkName: "dir-[request]" */
-        `./dir/${props.foo}/test`),
-
-    requireAsync(props) {
+    importAsync: (props)=>import(/*webpackChunkName: "dir-[request]"*/ `./dir/${props.foo}/test`),
+    requireAsync (props) {
         const key = this.resolve(props);
         this.resolved[key] = false;
-        return this.importAsync(props).then(resolved => {
+        return this.importAsync(props).then((resolved)=>{
             this.resolved[key] = true;
             return resolved;
         });
     },
-
-    requireSync(props) {
+    requireSync (props) {
         const id = this.resolve(props);
-
         if (typeof __webpack_require__ !== 'undefined') {
             return __webpack_require__(id);
         }
-
         return eval('module.require')(id);
     },
-
-    resolve(props) {
+    resolve (props) {
         if (require.resolveWeak) {
             return require.resolveWeak(`./dir/${props.foo}/test`);
         }
-
         return eval('require.resolve')(`./dir/${props.foo}/test`);
     }
-
 });
--- packages/loadable-components/tests/fixture/aggressive import/without "webpackChunkName"/should support destructuring/output.js
+++ packages/loadable-components/tests/fixture/aggressive import/without "webpackChunkName"/should support destructuring/output.js
@@ -1,59 +1,38 @@
 loadable({
     resolved: {},
-
-    chunkName({
-        foo
-    }) {
+    chunkName ({ foo  }) {
         return `dir-${foo}-test`.replace(/[^a-zA-Z0-9_!§$()=\\-^°]+/g, "-");
     },
-
-    isReady(props) {
+    isReady (props) {
         const key = this.resolve(props);
-
         if (this.resolved[key] !== true) {
             return false;
         }
-
         if (typeof __webpack_modules__ !== 'undefined') {
             return !!__webpack_modules__[key];
         }
-
         return false;
     },
-
-    importAsync: ({
-        foo
-    }) => import(
-        /* webpackChunkName: "dir-[request]" */
-        `./dir/${foo}/test`),
-
-    requireAsync(props) {
+    importAsync: ({ foo  })=>import(/*webpackChunkName: "dir-[request]"*/ `./dir/${foo}/test`),
+    requireAsync (props) {
         const key = this.resolve(props);
         this.resolved[key] = false;
-        return this.importAsync(props).then(resolved => {
+        return this.importAsync(props).then((resolved)=>{
             this.resolved[key] = true;
             return resolved;
         });
     },
-
-    requireSync(props) {
+    requireSync (props) {
         const id = this.resolve(props);
-
         if (typeof __webpack_require__ !== 'undefined') {
             return __webpack_require__(id);
         }
-
         return eval('module.require')(id);
     },
-
-    resolve({
-        foo
-    }) {
+    resolve ({ foo  }) {
         if (require.resolveWeak) {
             return require.resolveWeak(`./dir/${foo}/test`);
         }
-
         return eval('require.resolve')(`./dir/${foo}/test`);
     }
-
 });
