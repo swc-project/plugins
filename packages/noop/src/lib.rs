1571b7158f31e13dcfe94045d3e81d3b04db9db3
orionmiz <x3ph3r@naver.com>
2022-10-07 19:50:51 +0900

    fix(noop): Add missing imports

--- packages/noop/src/lib.rs
+++ packages/noop/src/lib.rs
@@ -1,3 +1,8 @@
+use swc_core::{
+    ecma::ast::Program,
+    plugin::{plugin_transform, proxies::TransformPluginProgramMetadata},
+};
+
 #[plugin_transform]
 fn noop(mut program: Program, _: TransformPluginProgramMetadata) -> Program {
     program
