cea85e837d934c354f762c69e7642e5ef6307689
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-08 12:13:57 +0900

    Move bump

--- scripts/bump-all.sh
+++ scripts/bump-all.sh
@@ -5,7 +5,17 @@ function bumpNpm {
     (cd $1 && yarn version --no-git-tag-version --patch)
 }
 
+function bumpCargo {
+    cargo mono bump $1 --breaking
+}
+
 for PKG in ./packages/*; do
     bumpNpm $PKG
 done
 
+
+bumpCargo swc_plugin_jest
+bumpCargo swc_plugin_styled_jsx
+bumpCargo swc_plugin_transform_imports
+bumpCargo swc_plugin_styled_components
+bumpCargo swc_plugin_emotion
--- scripts/update-bump-all.sh
+++ scripts/update-bump-all.sh
@@ -5,27 +5,11 @@ function up {
     cargo upgrade --workspace $@
 }
 
-function bump {
-    cargo mono bump $1 --breaking
-}
-
-# Delete tags
-git tag -d $(git tag -l)
-
 up swc_atoms swc_common testing swc_ecmascript swc_ecma_transforms_testing swc_plugin
 
 up styled_jsx styled_components modularize_imports swc_emotion
 
-bump swc_plugin_jest
-bump swc_plugin_styled_jsx
-bump swc_plugin_transform_imports
-bump swc_plugin_styled_components
-bump swc_plugin_emotion
-
 ./scripts/bump-all.sh
-
-# Delete tags
-git tag -d $(git tag -l)
 
 # Ensure that Cargo.lock is up-to-date
 cargo metadata --offline --format-version 1 > /dev/null
