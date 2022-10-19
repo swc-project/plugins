f7eb953af39df189db9183b5ce65b0d93c978875
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-19 13:15:02 +0900

    script

--- scripts/update-bump-all.sh
+++ scripts/update-bump-all.sh
@@ -5,7 +5,7 @@ function up {
     cargo upgrade --workspace $@
 }
 
-up swc_atoms swc_common testing swc_ecmascript swc_ecma_transforms_testing swc_plugin
+up swc_atoms swc_common testing swc_ecmascript swc_ecma_transforms_testing swc_plugin swc_core
 
 up styled_jsx styled_components modularize_imports swc_emotion
 
