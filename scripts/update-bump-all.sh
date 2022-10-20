a052d62eed6da3b72a34e5af4272a648f7246ef6
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-20 11:01:07 +0900

    git commit

--- scripts/update-bump-all.sh
+++ scripts/update-bump-all.sh
@@ -7,9 +7,15 @@ function up {
 
 up swc_atoms swc_common testing swc_ecmascript swc_ecma_transforms_testing swc_plugin swc_core
 
+git commit -a -m 'Update swc crates' || true
+
 up styled_jsx modularize_imports swc_emotion
+
+git commit -a -m 'Update extra crates' || true
 
 ./scripts/bump-all.sh
 
 # Ensure that Cargo.lock is up-to-date
 cargo metadata --offline --format-version 1 > /dev/null
+
+git commit -a -m 'lockfile' || true
