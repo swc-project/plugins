7ce4b5311aff02a624c1aa5b0a621d70f543e904
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-04 11:29:51 +0900

    Remove unneeded

--- crates/tailwind/tailwindcss-master/standalone-cli/tests/fixtures/test.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/forms')({ strategy: 'class' }),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/typography'),
-  ],
-}
