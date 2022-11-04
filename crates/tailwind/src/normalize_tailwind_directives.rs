ca65bae076d66042271ae6ef02daabcb2fe1e0ff
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-04 12:09:56 +0900

    Fix compiliation

--- crates/tailwind/src/normalize_tailwind_directives.rs
+++ crates/tailwind/src/normalize_tailwind_directives.rs
@@ -1,13 +1,44 @@
 use swc_core::{
     common::collections::AHashSet,
-    css::ast::{AtRule, Stylesheet},
+    css::{
+        ast::{AtRule, AtRuleName, Stylesheet},
+        visit::{Visit, VisitWith},
+    },
     ecma::atoms::JsWord,
 };
 
+#[derive(Default)]
 pub(crate) struct Directives {
     pub tailwindDirectives: AHashSet<JsWord>,
 
     pub applyDirectives: AHashSet<AtRule>,
 }
 
-pub(crate) fn normalize_tailwind_directives(ss: &Stylesheet) -> Directives {}
+pub(crate) fn normalize_tailwind_directives(ss: &Stylesheet) -> Directives {
+    let mut data = Directives::default();
+
+    let mut v = Visitor { data: &mut data };
+
+    data
+}
+
+struct Visitor<'a> {
+    data: &'a mut Directives,
+}
+
+impl Visit for Visitor<'_> {
+    fn visit_at_rule(&mut self, at_rule: &AtRule) {
+        if let AtRuleName::Ident(i) = &at_rule.name {
+            if &*i.value == "apply" {
+                self.data.applyDirectives.insert(at_rule.clone());
+            }
+
+            if &*i.value == "tailwind" {
+                // self.data.tailwindDirectives.insert(at_rule.params.
+                // clone());
+            }
+        }
+
+        at_rule.visit_children_with(self);
+    }
+}
