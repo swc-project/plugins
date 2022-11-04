c64086610094ec638606a871330e51aa5e3a5ffc
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-04 11:57:44 +0900

    This doesn't look important

--- crates/tailwind/src/detect_nesting.rs
+++ crates/tailwind/src/detect_nesting.rs
@@ -1,1 +1,25 @@
+#![allow(unused)]
 
+use swc_core::{
+    common::errors::HANDLER,
+    css::{
+        ast::{AtRule, Stylesheet},
+        visit::{Visit, VisitWith},
+    },
+};
+
+/// Emits error for `@tailwind` directives in nested rules.
+pub fn detect_nesting(ss: &Stylesheet) {
+    // ss.visit_with(&mut Detector {
+    //     in_nested_rule: false,
+    //     found: false,
+    // });
+}
+
+struct Detector {
+    in_nested_rule: bool,
+    found: bool,
+}
+
+// TODO(kdy1): Report error for nested css
+impl Visit for Detector {}
