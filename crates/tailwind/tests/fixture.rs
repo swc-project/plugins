449b7f3b6e8c516d0d90e15f649c6ff380c19ffc
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-04 15:47:47 +0900

    Test

--- crates/tailwind/tests/fixture.rs
+++ crates/tailwind/tests/fixture.rs
@@ -1,7 +1,12 @@
 use std::path::PathBuf;
 
-use swc_core::css::{ast::Stylesheet, parser::parser::ParserConfig};
+use swc_core::css::{
+    ast::Stylesheet,
+    codegen::{writer::basic::BasicCssWriter, CodeGenerator, Emit},
+    parser::parser::ParserConfig,
+};
 use swc_tailwind::Compiler;
+use testing::NormalizedOutput;
 
 #[testing::fixture("tests/fixture/**/input.css")]
 fn transform(input: PathBuf) {
@@ -9,7 +14,7 @@ fn transform(input: PathBuf) {
     let config_path = dir.join("config.json");
     let config = serde_json::from_str(&std::fs::read_to_string(&config_path).unwrap()).unwrap();
 
-    testing::run_test(false, |cm, handler| {
+    let result = testing::run_test(false, |cm, handler| {
         let fm = cm.load_file(&input).expect("failed to load input css file");
 
         let mut errors = vec![];
@@ -34,7 +39,32 @@ fn transform(input: PathBuf) {
 
         compiler.process(&mut ss);
 
-        Ok(())
-    })
-    .unwrap();
+        if handler.has_errors() {
+            return Err(());
+        }
+
+        Ok(ss)
+    });
+
+    match result {
+        Ok(ss) => {
+            let mut buf = String::new();
+            {
+                let mut g = CodeGenerator::new(
+                    BasicCssWriter::new(&mut buf, None, Default::default()),
+                    Default::default(),
+                );
+                g.emit(&ss).unwrap();
+            }
+
+            NormalizedOutput::from(buf)
+                .compare_to_file(input.with_extension("output.css"))
+                .unwrap();
+        }
+        Err(stderr) => {
+            stderr
+                .compare_to_file(dir.join("output.swc-stderr"))
+                .unwrap();
+        }
+    }
 }
