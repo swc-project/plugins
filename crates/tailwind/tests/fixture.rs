5caeb63fa0081f623efcc8f4c1a1ae99504f0664
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-04 15:41:01 +0900

    parse

--- crates/tailwind/tests/fixture.rs
+++ crates/tailwind/tests/fixture.rs
@@ -1,4 +1,33 @@
 use std::path::PathBuf;
 
+use swc_core::css::{ast::Stylesheet, parser::parser::ParserConfig};
+
 #[testing::fixture("tests/fixture/**/input.css")]
-fn transform(input: PathBuf) {}
+fn transform(input: PathBuf) {
+    let dir = input.parent().unwrap();
+
+    testing::run_test(false, |cm, handler| {
+        let fm = cm.load_file(&input).expect("failed to load input css file");
+
+        let mut errors = vec![];
+        let ss: Stylesheet = swc_core::css::parser::parse_file(
+            &fm,
+            ParserConfig {
+                ..Default::default()
+            },
+            &mut errors,
+        )
+        .expect("failed to parse input css file");
+
+        if !errors.is_empty() {
+            for err in errors {
+                err.to_diagnostics(&handler).emit();
+            }
+
+            return Err(());
+        }
+
+        Ok(())
+    })
+    .unwrap();
+}
