6565902942c27ae0f3a58a7a023f6a6e5029a175
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-04 11:37:31 +0900

    Config

--- crates/tailwind/src/lib.rs
+++ crates/tailwind/src/lib.rs
@@ -2,4 +2,7 @@ use swc_core::css::visit::VisitMut;
 
 pub struct Config {}
 
-pub fn tailwind() -> impl VisitMut {}
+/// Main entrypoint.
+///
+/// Note: We don't find config file here. It should be done by the caller.
+pub fn tailwind(c: Config) -> impl VisitMut {}
