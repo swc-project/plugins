a21571ac785cf57a7fb0dc212974c44c78298845
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-04 11:46:51 +0900

    mod

--- crates/tailwind/src/lib.rs
+++ crates/tailwind/src/lib.rs
@@ -1,8 +1,18 @@
 use swc_core::css::visit::VisitMut;
 
+mod detect_nesting;
+
 pub struct Config {}
 
 /// Main entrypoint.
 ///
 /// Note: We don't find config file here. It should be done by the caller.
-pub fn tailwind(c: Config) -> impl VisitMut {}
+pub struct Compiler {
+    config: Config,
+}
+
+impl Compiler {
+    pub fn new(config: Config) -> Self {
+        Self { config }
+    }
+}
