f15f48cf6924b4bb0dfeb59fa0ea9eab8042ef13
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-04 15:16:22 +0900

    Move

--- crates/tailwind/tests/fixture/basic-usage/basic-usage.test.js
+++ crates/tailwind/tests/fixture/basic-usage/basic-usage.test.js
@@ -3,26 +3,6 @@ import path from 'path'
 
 import { html, run, css, defaults } from '../../../tailwindcss-master/tests/util/run'
 
-test('basic usage', () => {
-  let config = {
-    content: [path.resolve(__dirname, './basic-usage.test.html')],
-    corePlugins: { preflight: false },
-  }
-
-  let input = css`
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
-  `
-
-  return run(input, config).then((result) => {
-    let expectedPath = path.resolve(__dirname, './basic-usage.test.css')
-    let expected = fs.readFileSync(expectedPath, 'utf8')
-
-    expect(result.css).toMatchFormattedCss(expected)
-  })
-})
-
 test('all plugins are executed that match a candidate', () => {
   let config = {
     content: [{ raw: html`<div class="bg-green-light bg-green"></div>` }],
