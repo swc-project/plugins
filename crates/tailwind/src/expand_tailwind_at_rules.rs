806454dc0fb9f8fc600e96ab8595622344cd756d
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-07 13:17:40 +0900

    layers

--- crates/tailwind/src/expand_tailwind_at_rules.rs
+++ crates/tailwind/src/expand_tailwind_at_rules.rs
@@ -1,13 +1,50 @@
-use swc_core::css::{
-    ast::Stylesheet,
-    visit::{VisitMut, VisitMutWith},
+use swc_core::{
+    common::collections::AHashSet,
+    css::{
+        ast::{AtRule, AtRuleName, Stylesheet},
+        visit::{Visit, VisitWith},
+    },
 };
 
 pub(crate) fn expand_tailwind_at_rules(ss: &mut Stylesheet) {
-    ss.visit_mut_with(&mut TailwindExpander::default());
+    let mut layers = AHashSet::<LayerNode>::default();
+
+    ss.visit_with(&mut TailwindFinder {
+        layers: &mut layers,
+    });
+
+    if layers.is_empty() {
+        return;
+    }
+
+    //
 }
 
-#[derive(Debug, Default)]
-struct TailwindExpander {}
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum LayerNode {
+    Base,
+    Components,
+    Utilities,
+    Variant,
+}
 
-impl VisitMut for TailwindExpander {}
+struct TailwindFinder<'a> {
+    layers: &'a mut AHashSet<LayerNode>,
+}
+
+impl Visit for TailwindFinder<'_> {
+    fn visit_at_rule(&mut self, n: &AtRule) {
+        n.visit_children_with(self);
+
+        match &n.name {
+            AtRuleName::Ident(n) => {
+                if &*n.value != "tailwind" {
+                    return;
+                }
+            }
+            _ => return,
+        }
+
+        //
+    }
+}
