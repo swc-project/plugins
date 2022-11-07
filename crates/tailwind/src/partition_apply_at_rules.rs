31b0ed386f2f7523dd0b31862f70eac0fe4b12c2
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-07 11:42:01 +0900

    More

--- crates/tailwind/src/partition_apply_at_rules.rs
+++ crates/tailwind/src/partition_apply_at_rules.rs
@@ -1,3 +1,13 @@
-use swc_core::css::ast::Stylesheet;
+use swc_core::css::{
+    ast::Stylesheet,
+    visit::{VisitMut, VisitMutWith},
+};
 
-pub fn partition_apply_at_rules(ss: &mut Stylesheet) {}
+pub fn partition_apply_at_rules(ss: &mut Stylesheet) {
+    ss.visit_mut_with(&mut Visitor::default());
+}
+
+#[derive(Debug, Default)]
+struct Visitor {}
+
+impl VisitMut for Visitor {}
