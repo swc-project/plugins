b7233c53248eb61c0334e4fe1505f9f79b5fa269
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-08 11:09:39 +0900

    More work

---
+++ crates/tailwind/src/evaluate_tailwind_functions.rs
@@ -0,0 +1,5 @@
+use swc_core::css::ast::Stylesheet;
+
+use crate::context::Context;
+
+pub fn evaluate_tailwind_functions(context: &mut Context, ss: &mut Stylesheet) {}
---
+++ crates/tailwind/src/expand_apply_at_rules.rs
@@ -0,0 +1,5 @@
+use swc_core::css::ast::Stylesheet;
+
+use crate::context::Context;
+
+pub fn expand_apply_at_rules(context: &mut Context, ss: &mut Stylesheet) {}
