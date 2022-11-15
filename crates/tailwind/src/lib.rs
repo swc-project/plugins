4b7306529902def3e0bfdc3341b0090ba7c86304
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-15 10:35:23 +0900

    candidates

--- crates/tailwind/src/lib.rs
+++ crates/tailwind/src/lib.rs
@@ -1,6 +1,11 @@
-use std::path::{Path, PathBuf};
+use std::{
+    fs::read_to_string,
+    path::{Path, PathBuf},
+};
 
-use anyhow::Result;
+use anyhow::{Context, Result};
+use regex::Regex;
+use swc_common::{collections::AHashSet, sync::Lazy};
 
 /// Content of the config file
 #[derive(Debug)]
@@ -14,6 +19,8 @@ impl Config {
     }
 }
 
+static CONTENT_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r#"['"\s<>=/]"#).unwrap());
+
 #[derive(Debug)]
 pub struct Tailwind {
     config_path: PathBuf,
@@ -25,8 +32,33 @@ impl Tailwind {
     }
 
     pub fn compile(&mut self) -> Result<()> {
-        let config = Config::from_path(&self.config_path)?;
+        let config = Config::from_path(&self.config_path).context("failed to load config file")?;
+
+        let files = resolve_glob(&config.content);
+
+        // TODO: Make this parallel using rayon once wasm get support for it
+        // TODO: Optimize
+        let candidates = files
+            .into_iter()
+            .map(|f| {
+                let contents = read_to_string(&f).context("failed to read file")?;
+
+                let s = CONTENT_RE
+                    .split(&contents)
+                    .map(|s| s.to_string())
+                    .collect::<Vec<_>>();
+
+                Ok(s)
+            })
+            .collect::<Result<Vec<_>>>()?
+            .into_iter()
+            .flatten()
+            .collect::<AHashSet<_>>();
 
         Ok(())
     }
+}
+
+fn resolve_glob(config: &[String]) -> Vec<PathBuf> {
+    todo!()
 }
