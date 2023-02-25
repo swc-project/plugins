d0ed05c8bcc0dabe532f49b8dd45ac3d01c9eca3
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-25 22:54:44 +0900

    lint

--- packages/relay/tests/fixture.rs
+++ packages/relay/tests/fixture.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 use swc_common::FileName;
-use swc_core::ecma::{transforms::testing::test_fixture, visit::as_folder};
+use swc_core::ecma::transforms::testing::test_fixture;
 use swc_plugin_relay::{relay, Config, RelayLanguageConfig};
 
 #[testing::fixture("tests/fixture/simple/**/input.js")]
@@ -10,15 +10,17 @@ fn fixture(input: PathBuf) {
 
     test_fixture(
         Default::default(),
-        &|_| relay(
-            &Config {
-                artifact_directory: None,
-                language: RelayLanguageConfig::TypeScript,
-                eager_es_modules: false,
-            },
-            FileName::Real("file.js".parse().unwrap()),
-            Default::default(),
-        ),
+        &|_| {
+            relay(
+                &Config {
+                    artifact_directory: None,
+                    language: RelayLanguageConfig::TypeScript,
+                    eager_es_modules: false,
+                },
+                FileName::Real("file.js".parse().unwrap()),
+                Default::default(),
+            )
+        },
         &input,
         &output,
         Default::default(),
@@ -31,15 +33,17 @@ fn fixture_es_modules(input: PathBuf) {
 
     test_fixture(
         Default::default(),
-        &|_| relay(
-            &Config {
-                artifact_directory: None,
-                language: RelayLanguageConfig::TypeScript,
-                eager_es_modules: true,
-            },
-            FileName::Real("file.js".parse().unwrap()),
-            Default::default(),
-        ),
+        &|_| {
+            relay(
+                &Config {
+                    artifact_directory: None,
+                    language: RelayLanguageConfig::TypeScript,
+                    eager_es_modules: true,
+                },
+                FileName::Real("file.js".parse().unwrap()),
+                Default::default(),
+            )
+        },
         &input,
         &output,
         Default::default(),
