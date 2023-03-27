bb8f9a4b30a7130f50582f681db858ff98df921f
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-27 16:37:16 +0900

    build(cargo): Update `swc_core` to `v0.72.0` (#165)

--- packages/loadable-components/tests/fixture/aggressive import/with webpackChunkName/should keep it/output.js
+++ packages/loadable-components/tests/fixture/aggressive import/with webpackChunkName/should keep it/output.js
@@ -1,25 +1,25 @@
 loadable({
     resolved: {},
-    chunkName (props) {
-        return "pages/" + props.path.replace(/[^a-zA-Z0-9_!§$()=\\-^°]+/g, "-");
+    chunkName (props1) {
+        return "pages/" + props1.path.replace(/[^a-zA-Z0-9_!§$()=\\-^°]+/g, "-");
     },
-    isReady (props) {
-        const key = this.resolve(props);
-        if (this.resolved[key] !== true) {
+    isReady (props1) {
+        const key1 = this.resolve(props1);
+        if (this.resolved[key1] !== true) {
             return false;
         }
         if (typeof __webpack_modules__ !== 'undefined') {
-            return !!__webpack_modules__[key];
+            return !!__webpack_modules__[key1];
         }
         return false;
     },
-    importAsync: (props)=>import(/*webpackChunkName: "pages/[request]"*/ `./pages/${props.path}`),
-    requireAsync (props) {
-        const key = this.resolve(props);
-        this.resolved[key] = false;
-        return this.importAsync(props).then((resolved)=>{
-            this.resolved[key] = true;
-            return resolved;
+    importAsync: (props1)=>import(/*webpackChunkName: "pages/[request]"*/ `./pages/${props1.path}`),
+    requireAsync (props1) {
+        const key1 = this.resolve(props1);
+        this.resolved[key1] = false;
+        return this.importAsync(props1).then((resolved1)=>{
+            this.resolved[key1] = true;
+            return resolved1;
         });
     },
     requireSync (props) {
--- packages/loadable-components/tests/fixture/simple import/should work with plus concatenation/output.js
+++ packages/loadable-components/tests/fixture/simple import/should work with plus concatenation/output.js
@@ -3,23 +3,23 @@ loadable({
     chunkName () {
         return "";
     },
-    isReady (props) {
-        const key = this.resolve(props);
-        if (this.resolved[key] !== true) {
+    isReady (props1) {
+        const key1 = this.resolve(props1);
+        if (this.resolved[key1] !== true) {
             return false;
         }
         if (typeof __webpack_modules__ !== 'undefined') {
-            return !!__webpack_modules__[key];
+            return !!__webpack_modules__[key1];
         }
         return false;
     },
     importAsync: ()=>import(/*webpackChunkName: ""*/ './Mod' + 'A'),
-    requireAsync (props) {
-        const key = this.resolve(props);
-        this.resolved[key] = false;
-        return this.importAsync(props).then((resolved)=>{
-            this.resolved[key] = true;
-            return resolved;
+    requireAsync (props1) {
+        const key1 = this.resolve(props1);
+        this.resolved[key1] = false;
+        return this.importAsync(props1).then((resolved1)=>{
+            this.resolved[key1] = true;
+            return resolved1;
         });
     },
     requireSync (props) {
