42812c688306899cb4b9f2cfa3253a68f91fd26c
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-11 13:46:52 +0900

    ctxt

--- packages/constify/transform/src/lib.rs
+++ packages/constify/transform/src/lib.rs
@@ -1,11 +1,26 @@
-use swc_core::ecma::visit::{noop_visit_mut_type, VisitMut};
+use swc_core::{
+    common::{Mark, SyntaxContext},
+    ecma::{
+        ast::Stmt,
+        visit::{noop_visit_mut_type, VisitMut},
+    },
+};
 
 pub fn constify() -> impl VisitMut {
-    Constify::default()
+    Constify {
+        const_ctxt: SyntaxContext::empty().apply_mark(Mark::new()),
+        next_const_id: 0,
+        prepend_stmts: vec![],
+    }
 }
 
-#[derive(Default)]
-struct Constify {}
+struct Constify {
+    const_ctxt: SyntaxContext,
+
+    next_const_id: u32,
+
+    prepend_stmts: Vec<Stmt>,
+}
 
 impl VisitMut for Constify {
     noop_visit_mut_type!();
