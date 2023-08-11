7d0ed74f9be204e8611894d9fe186bc47d45a263
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-11 14:49:45 +0900

    Add a test

---
+++ packages/styled-jsx/transform/tests/fixture/transform-css-complex-selector-2/input.js
@@ -0,0 +1,12 @@
+export default () => (
+  <div>
+    <p>test</p>
+    <style jsx>{`
+      .a-selector:hover,
+      .a-selector:focus
+      {
+        outline: none;
+      }   
+    `}</style>
+  </div>
+)
