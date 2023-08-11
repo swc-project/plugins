a5be2cc39c1dd260badc62995b22432c9dc404fa
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-11 16:02:45 +0900

    Update test refs

--- packages/styled-jsx/transform/tests/fixture/transform-css-global/output.js
+++ packages/styled-jsx/transform/tests/fixture/transform-css-global/output.js
@@ -3,6 +3,6 @@ export default (()=><div className={"jsx-53fd644ab080300c"}>
 
     <p className={"jsx-53fd644ab080300c"}>test</p>
 
-    <_JSXStyle id={"53fd644ab080300c"}>{"html.jsx-53fd644ab080300c{background-image:linear-gradient(#fffc,#fffc),url(/static/background.svg)}p{color:#00f}a.jsx-53fd644ab080300c{color:#00f}p{color:#00f}a{color:red}body{font-family:-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif}"}</_JSXStyle>
+    <_JSXStyle id={"53fd644ab080300c"}>{"html.jsx-53fd644ab080300c{background-image:linear-gradient(#fffc,#fffc),url(/static/background.svg)}p{color:#00f}a.jsx-53fd644ab080300c{color:#00f}p{color:#00f}a+.foo{color:red}body{font-family:-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif}"}</_JSXStyle>
 
   </div>);
