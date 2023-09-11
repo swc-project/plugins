b58f33f3d894a18e7219d2f0b66586b8a977dd68
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-09-11 13:47:35 +0900

    Update test refs

---
+++ packages/styled-jsx/transform/tests/fixture/global/output.lightningcss.js
@@ -0,0 +1,7 @@
+import _JSXStyle from "styled-jsx/style";
+const Test = ()=><div className={"jsx-d47d6adadf14e957"}>
+
+    <_JSXStyle id={"d47d6adadf14e957"}>{"body{color:red}:hover{color:red;animation:1s ease-out foo;display:flex}div a{display:none}[data-test]>div{color:red}"}</_JSXStyle>
+
+  </div>;
+const Test2 = ()=><_JSXStyle id={"94239b6d6b42c9b5"}>{"p{color:red}"}</_JSXStyle>;
---
+++ packages/styled-jsx/transform/tests/fixture/issue-30480/output.swc.js
@@ -0,0 +1,15 @@
+import _JSXStyle from "styled-jsx/style";
+export default (({ breakPoint })=><div className={_JSXStyle.dynamic([
+        [
+            "ff798a455a83a572",
+            [
+                breakPoint
+            ]
+        ]
+    ])}>
+
+    <_JSXStyle id={"ff798a455a83a572"} dynamic={[
+        breakPoint
+    ]}>{`@media(${breakPoint}){.test.__jsx-style-dynamic-selector{margin-bottom:1em}}`}</_JSXStyle>
+
+  </div>);
