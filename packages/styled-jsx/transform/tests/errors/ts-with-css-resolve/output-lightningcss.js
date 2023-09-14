29e924fffd888285ddf280220eb4d1edb92b2e0f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-15 02:38:07 +0900

    Update test refs

---
+++ packages/styled-jsx/transform/tests/errors/ts-with-css-resolve/output-lightningcss.js
@@ -0,0 +1,5 @@
+import _JSXStyle from "styled-jsx/style";
+export default {
+    styles: <_JSXStyle id={"71f03d42ea0ec6"}>{".container.jsx-71f03d42ea0ec6{color:#fff;background:#000;height:100px;font-weight:700}"}</_JSXStyle>,
+    className: "jsx-71f03d42ea0ec6"
+};
