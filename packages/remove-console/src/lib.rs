cc498f773593905138185809e89a8c530bb8a535
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-15 17:44:48 +0900

    fixup

--- packages/remove-console/src/lib.rs
+++ packages/remove-console/src/lib.rs
@@ -5,7 +5,7 @@ use swc_core::{
 };
 
 #[plugin_transform]
-fn transform_imports_plugin(program: Program, data: TransformPluginProgramMetadata) -> Program {
+fn remove_console_plugin(program: Program, data: TransformPluginProgramMetadata) -> Program {
     let packages = serde_json::from_str(
         &data
             .get_transform_plugin_config()
