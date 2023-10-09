032312bad77f5e65b644cc9feb86914b00aadfe6
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-10 01:47:02 +0900

    cargo metadata

--- xtask/src/update.rs
+++ xtask/src/update.rs
@@ -1,9 +1,22 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
+use cargo_metadata::MetadataCommand;
 use clap::Args;
 
 #[derive(Debug, Args)]
 pub struct Update {}
 
 impl Update {
-    pub fn run(self) -> Result<()> {}
+    pub fn run(self) -> Result<()> {
+        update_swc_crates().context("failed to update swc crates")?;
+
+        Ok(())
+    }
+}
+
+fn update_swc_crates() -> Result<()> {
+    let md = MetadataCommand::new().exec()?;
+
+    dbg!(&md);
+
+    Ok(())
 }
