3d788f223fe417d03edf2470972ac1fbe5ae7ccd
Julien Karst <contact@julienkarst.com>
2023-10-24 21:08:14 +0200

    test: add outputFileExtension to specify which output extension to use

--- packages/relay/transform/tests/fixture.rs
+++ packages/relay/transform/tests/fixture.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use swc_common::FileName;
 use swc_ecma_transforms_testing::test_fixture;
-use swc_relay::{relay, Config, RelayLanguageConfig};
+use swc_relay::{relay, Config, RelayLanguageConfig, OutputFileExtension};
 
 #[testing::fixture("tests/fixture/simple/**/input.js")]
 fn fixture(input: PathBuf) {
@@ -16,6 +16,7 @@ fn fixture(input: PathBuf) {
                     artifact_directory: None,
                     language: RelayLanguageConfig::TypeScript,
                     eager_es_modules: false,
+                    output_file_extension: OutputFileExtension::Undefined,
                 },
                 FileName::Real("file.js".parse().unwrap()),
                 Default::default(),
@@ -41,6 +42,59 @@ fn fixture_es_modules(input: PathBuf) {
                     artifact_directory: None,
                     language: RelayLanguageConfig::TypeScript,
                     eager_es_modules: true,
+                    output_file_extension: OutputFileExtension::Undefined,
+                },
+                FileName::Real("file.js".parse().unwrap()),
+                Default::default(),
+                None,
+                None,
+            )
+        },
+        &input,
+        &output,
+        Default::default(),
+    );
+}
+
+#[testing::fixture("tests/fixture/outputFileExtension/javascript/**/input.js")]
+fn fixture_output_file_extension_javascript(input: PathBuf) {
+    let output = input.parent().unwrap().join("output.js");
+
+    test_fixture(
+        Default::default(),
+        &|_| {
+            relay(
+                &Config {
+                    artifact_directory: None,
+                    language: RelayLanguageConfig::TypeScript,
+                    eager_es_modules: true,
+                    output_file_extension: OutputFileExtension::JavaScript,
+                },
+                FileName::Real("file.js".parse().unwrap()),
+                Default::default(),
+                None,
+                None,
+            )
+        },
+        &input,
+        &output,
+        Default::default(),
+    );
+}
+
+#[testing::fixture("tests/fixture/outputFileExtension/typescript/**/input.js")]
+fn fixture_output_file_extension_typescript(input: PathBuf) {
+    let output = input.parent().unwrap().join("output.js");
+
+    test_fixture(
+        Default::default(),
+        &|_| {
+            relay(
+                &Config {
+                    artifact_directory: None,
+                    language: RelayLanguageConfig::JavaScript,
+                    eager_es_modules: true,
+                    output_file_extension: OutputFileExtension::TypeScript,
                 },
                 FileName::Real("file.js".parse().unwrap()),
                 Default::default(),
