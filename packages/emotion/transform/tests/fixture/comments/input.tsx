ad70412258c51de829882ba4d44bf3d9ad244b72
jserfeng <1114550440@qq.com>
2023-11-07 00:06:35 +0800

    chore(emotion): add tests

---
+++ packages/emotion/transform/tests/fixture/comments/input.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export default styled.div`
+  color: red;
+  .foo {
+    /**
+      multi line comments
+    */
+  }
+`;
---
+++ packages/emotion/transform/tests/fixture/comments/output.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+export default /*#__PURE__*/ styled("div", {
+  target: "e1gokyz70",
+  label: "[local]",
+})(
+  "color:red;.foo{}",
+  "/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5wdXQudHMiLCJzb3VyY2VzIjpbImlucHV0LnRzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBzdHlsZWQgZnJvbSBcIkBlbW90aW9uL3N0eWxlZFwiO1xuXG5leHBvcnQgZGVmYXVsdCBzdHlsZWQuZGl2YFxuICBjb2xvcjogcmVkO1xuICAuZm9vIHtcbiAgICAvKipcbiAgICAgIG11bHRpIGxpbmUgY29tbWVudHNcbiAgICAqL1xuICB9XG5gO1xuIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUVlIn0= */"
+);
