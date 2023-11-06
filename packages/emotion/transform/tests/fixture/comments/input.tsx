c3dafac2d13faac690e1b5085cc5fbc24ea5a0fc
jserfeng <1114550440@qq.com>
2023-11-07 00:10:10 +0800

    chore(emotion): add tests

--- packages/emotion/transform/tests/fixture/comments/input.tsx
+++ packages/emotion/transform/tests/fixture/comments/input.tsx
@@ -3,8 +3,11 @@ import styled from "@emotion/styled";
 export default styled.div`
   color: red;
   .foo {
+    color: blue;
     /**
       multi line comments
     */
   }
+  /* /* */
+  width: 10px;
 `;
