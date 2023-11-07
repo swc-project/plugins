02f982281c04200f78143d5d8c04b33891f7aced
Fy <1114550440@qq.com>
2023-11-07 08:36:53 +0800

    fix(emotion): Remove multiline comments (#229)
    This is because emotion@10 runtime cannot handle multiline comments properly, the @emotion/babel-plugin removes comments as well.

---
+++ packages/emotion/transform/tests/fixture/comments/input.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export default styled.div`
+  color: red;
+  .foo {
+    color: blue;
+    /**
+      multi line comments
+    */
+  }
+  /* /* */
+  width: 10px;
+`;
