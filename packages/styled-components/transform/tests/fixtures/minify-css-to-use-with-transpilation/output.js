4f96bd44e2c4adda86f68ccffd5961d5b9611f13
Ciffelia <mc.prince.0203@gmail.com>
2023-11-22 04:26:04 +0900

    update test fixtures output

--- packages/styled-components/transform/tests/fixtures/minify-css-to-use-with-transpilation/output.js
+++ packages/styled-components/transform/tests/fixtures/minify-css-to-use-with-transpilation/output.js
@@ -1,23 +1,6 @@
-"use strict";
-
-var _styledComponents = _interopRequireDefault(require("styled-components"));
-
-var _templateObject, _templateObject2, _templateObject3, _templateObject4, _templateObject5;
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
-
-var Simple = _styledComponents["default"].div(_templateObject || (_templateObject = _taggedTemplateLiteral(["width:100%;"])));
-
-var Interpolation = _styledComponents["default"].div(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["content:\"  ", "  \";"])), function (props) {
-  return props.text;
-});
-
-var SpecialCharacters = _styledComponents["default"].div(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["content:\"  ", "  \";color:red;"])), function (props) {
-  return props.text;
-});
-
-var Comment = _styledComponents["default"].div(_templateObject4 || (_templateObject4 = _taggedTemplateLiteral(["color:red;"])));
-
-var Parens = _styledComponents["default"].div(_templateObject5 || (_templateObject5 = _taggedTemplateLiteral(["&:hover{color:blue;}"])));
+import styled from 'styled-components';
+const Simple = styled.div`width:100%;`;
+const Interpolation = styled.div`content:"  ${(props)=>props.text}  ";`;
+const SpecialCharacters = styled.div`content:"  ${(props)=>props.text}  ";color:red;`;
+const Comment = styled.div`color:red;`;
+const Parens = styled.div`&:hover{color:blue;}`;
