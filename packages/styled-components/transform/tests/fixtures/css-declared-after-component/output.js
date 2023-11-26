53c9ba54d671894c3a99f6ed688643d9c7f84397
Ciffelia <mc.prince.0203@gmail.com>
2023-11-27 00:25:02 +0900

    stop ignoring some tests

--- packages/styled-components/transform/tests/fixtures/css-declared-after-component/output.js
+++ packages/styled-components/transform/tests/fixtures/css-declared-after-component/output.js
@@ -2,11 +2,15 @@ import _styled from "styled-components";
 import React from "react";
 import { css } from "styled-components";
 export default function Example() {
-  return <_StyledDiv>oops</_StyledDiv>;
+    return <_StyledDiv>oops</_StyledDiv>;
 }
-const someCss = css(["color:red;"]);
-
+const someCss = css([
+    "color:red;"
+]);
 var _StyledDiv = _styled("div").withConfig({
-  displayName: "code___StyledDiv",
-  componentId: "sc-7mydya-0"
-})(["", ""], someCss);
+    displayName: "code___StyledDiv",
+    componentId: "sc-216c6228-0"
+})([
+    "",
+    ""
+], someCss);
