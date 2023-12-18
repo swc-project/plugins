c51a30fb99250b965d16c93a0d487d9dee7de040
themezv <artem-zverev@yandex-team.ru>
2023-12-18 14:49:53 +0100

    test: Add tests on lazy from loadable-components

---
+++ packages/loadable-components/tests/fixture/lazy/should work with renamed specifier/output.js
@@ -0,0 +1,40 @@
+import { lazy as renamedLazy } from "@loadable/component";
+
+renamedLazy({
+  resolved: {},
+  chunkName() {
+    return "ModA";
+  },
+  isReady(props) {
+    const key = this.resolve(props);
+    if (this.resolved[key] !== true) {
+      return false;
+    }
+    if (typeof __webpack_modules__ !== "undefined") {
+      return !!__webpack_modules__[key];
+    }
+    return false;
+  },
+  importAsync: () => import(/*webpackChunkName: "ModA"*/ "./ModA"),
+  requireAsync(props) {
+    const key = this.resolve(props);
+    this.resolved[key] = false;
+    return this.importAsync(props).then((resolved) => {
+      this.resolved[key] = true;
+      return resolved;
+    });
+  },
+  requireSync(props) {
+    const id = this.resolve(props);
+    if (typeof __webpack_require__ !== "undefined") {
+      return __webpack_require__(id);
+    }
+    return eval("module.require")(id);
+  },
+  resolve() {
+    if (require.resolveWeak) {
+      return require.resolveWeak("./ModA");
+    }
+    return eval("require.resolve")("./ModA");
+  },
+});
