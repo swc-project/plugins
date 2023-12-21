f183355c4a565c15fc77c87dd75b7c953a082870
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-12-22 04:57:04 +0900

    order

--- scripts/bump-all.sh
+++ scripts/bump-all.sh
@@ -13,13 +13,14 @@ CRATES="$(cargo metadata --format-version 1 \
     | jq -r '.packages[] | select(.source == null) | .name')"
 
 
-for PKG in ./packages/*; do
-    bumpNpm $PKG
-    git commit -a -m "Bump npm package: ${PKG}" || true
-done
 
 for CRATE in $CRATES
 do
    bumpCargo $CRATE
    git commit -a -m "Bump cargo crate: ${CRATE}" || true
+done
+
+for PKG in ./packages/*; do
+    bumpNpm $PKG
+    git commit -a -m "Bump npm package: ${PKG}" || true
 done
