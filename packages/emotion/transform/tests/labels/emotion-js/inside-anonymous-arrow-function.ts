51f3f2e2431065c59271f0766efc74aec1e4d5c5
Alex Miller <codex.nz@gmail.com>
2024-01-13 02:28:13 +1300

    test: port more emotion tests

---
+++ packages/emotion/transform/tests/labels/emotion-js/inside-anonymous-arrow-function.ts
@@ -0,0 +1,9 @@
+// https://github.com/emotion-js/emotion/blob/main/packages/babel-plugin/__tests__/css-macro/__fixtures__/inside-anonymous-arrow-function.js
+
+import { css } from "@emotion/react";
+
+export default () => {
+  css`
+    color: hotpink;
+  `;
+};
