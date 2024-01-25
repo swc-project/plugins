3b2b5eedfabb12ffed93af1f7b1b0e68029f1018
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-25 10:45:06 +0900

    Update test refs

--- packages/styled-jsx/transform/tests/fixture-swc-only/next-55679/output.swc.js
+++ packages/styled-jsx/transform/tests/fixture-swc-only/next-55679/output.swc.js
@@ -4,7 +4,7 @@ const someVar = "--var-1";
 export default function App({ Component, pageProps }) {
     return <>
 
-            <_JSXStyle id={"caeea3e7c6bbdab1"}>{`:root{${someVar}:red;background-color:var(${someVar})}`}</_JSXStyle>
+            <_JSXStyle id={"caeea3e7c6bbdab1"}>{`:root{${someVar} red;background-color:var(${someVar})}`}</_JSXStyle>
 
             <Component {...pageProps} className={"jsx-caeea3e7c6bbdab1" + " " + (pageProps && pageProps.className != null && pageProps.className || "")}/>
 
