bfa91f6d842d26734dbff5bde6864509c67bc452
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-31 12:20:53 +0900

    Reduce test input

--- packages/emotion/transform/tests/fixture/next/40385/1/input.tsx
+++ packages/emotion/transform/tests/fixture/next/40385/1/input.tsx
@@ -1,31 +1,19 @@
 import styled from "@emotion/styled";
 
 export default function IndexPage() {
-    return (
-        <>
-            <h1>IndexPage</h1>
-            <IconWrapper className={'icon-chat'} />
-            <IconWrapper className={'icon-check'} />
-        </>
-    );
+  return (
+    <>
+      <h1>IndexPage</h1>
+      <IconWrapper className={"icon-chat"} />
+      <IconWrapper className={"icon-check"} />
+    </>
+  );
 }
 
 const IconWrapper = styled.div`
-  &[class^="icon-"], [class*=" icon-"] {
-    /* use !important to prevent issues with browser extensions that change fonts */
-    font-family: 'icomoon' !important;
-    speak: never;
-    font-style: normal;
-    font-weight: 400;
-    font-variant: normal;
-    text-transform: none;
-    font-size: 17px;
-    line-height: 17px;
+  &[class^="icon-"],
+  [class*=" icon-"] {
     color: red;
-
-    /* Better Font Rendering =========== */
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
   }
 
   &.icon-chat:before {
