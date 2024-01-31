a72c844ab8a31eaca3a0856b3586039424d83f4a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-31 12:21:00 +0900

    Update test refs

---
+++ packages/emotion/transform/tests/fixture/next/40385/1/output.ts
@@ -0,0 +1,21 @@
+import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
+import styled from "@emotion/styled";
+export default function IndexPage() {
+    return /*#__PURE__*/ _jsxs(_Fragment, {
+        children: [
+            /*#__PURE__*/ _jsx("h1", {
+                children: "IndexPage"
+            }),
+            /*#__PURE__*/ _jsx(IconWrapper, {
+                className: "icon-chat"
+            }),
+            /*#__PURE__*/ _jsx(IconWrapper, {
+                className: "icon-check"
+            })
+        ]
+    });
+}
+const IconWrapper = /*#__PURE__*/ styled("div", {
+    target: "ewuh3ed0",
+    label: "IconWrapper"
+})('&[class^="icon-"],[class*=" icon-"]{color:red;}&.icon-chat:before{content:"\\e904";}&.icon-check:before{content:"\\e905";}', "/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5wdXQudHMiLCJzb3VyY2VzIjpbImlucHV0LnRzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBzdHlsZWQgZnJvbSBcIkBlbW90aW9uL3N0eWxlZFwiO1xuXG5leHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBJbmRleFBhZ2UoKSB7XG4gIHJldHVybiAoXG4gICAgPD5cbiAgICAgIDxoMT5JbmRleFBhZ2U8L2gxPlxuICAgICAgPEljb25XcmFwcGVyIGNsYXNzTmFtZT17XCJpY29uLWNoYXRcIn0gLz5cbiAgICAgIDxJY29uV3JhcHBlciBjbGFzc05hbWU9e1wiaWNvbi1jaGVja1wifSAvPlxuICAgIDwvPlxuICApO1xufVxuXG5jb25zdCBJY29uV3JhcHBlciA9IHN0eWxlZC5kaXZgXG4gICZbY2xhc3NePVwiaWNvbi1cIl0sXG4gIFtjbGFzcyo9XCIgaWNvbi1cIl0ge1xuICAgIGNvbG9yOiByZWQ7XG4gIH1cblxuICAmLmljb24tY2hhdDpiZWZvcmUge1xuICAgIGNvbnRlbnQ6IFwiXFxcXGU5MDRcIjtcbiAgfVxuXG4gICYuaWNvbi1jaGVjazpiZWZvcmUge1xuICAgIGNvbnRlbnQ6IFwiXFxcXGU5MDVcIjtcbiAgfVxuYDtcbiJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFZb0IifQ== */");
