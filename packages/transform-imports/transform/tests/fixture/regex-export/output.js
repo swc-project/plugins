5991cc6e35a2081a3ea0bb28a194523182043d9c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-19 14:43:39 +0900

    Update test rfs

---
+++ packages/transform-imports/transform/tests/fixture/regex-export/output.js
@@ -0,0 +1,4 @@
+export { default as MyModule } from "my-library/MyModule";
+export { default as App } from "my-library/components/App";
+export { default as Header } from "my-library/components/App/Header";
+export { default as Footer } from "my-library/components/App/Footer";
