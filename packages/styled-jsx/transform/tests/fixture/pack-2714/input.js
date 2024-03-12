4e0086f436d6612eef7d2a7e7c30a6a5ad0b496a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-03-12 14:53:14 +0900

    Reduce

--- packages/styled-jsx/transform/tests/fixture/pack-2714/input.js
+++ packages/styled-jsx/transform/tests/fixture/pack-2714/input.js
@@ -2,13 +2,6 @@ import css from 'styled-jsx/css';
 
 
 const { className: cardClassName, styles } = css.resolve`
-.geist-card {
-    padding: ${padding ? 'var(--geist-gap)' : '0'};
-    border-radius: var(--geist-radius);
-    background: var(--geist-background);
-    box-shadow: ${shadow ? 'var(--ds-shadow-border-small)' : 'none'};
-    transition: box-shadow 0.2s ease;
-}
 :hover {
     z-index: ${hoverAnimation ? '1' : 'auto'};
 }
