7336b9a73211211e78561eee3ffc6e1cd29f60c5
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-03-12 13:33:14 +0900

    Add a test

---
+++ packages/styled-jsx/transform/tests/fixture/pack-2714/input.js
@@ -0,0 +1,12 @@
+const { className: cardClassName, styles } = css.resolve`
+.geist-card {
+    padding: ${padding ? 'var(--geist-gap)' : '0'};
+    border-radius: var(--geist-radius);
+    background: var(--geist-background);
+    box-shadow: ${shadow ? 'var(--ds-shadow-border-small)' : 'none'};
+    transition: box-shadow 0.2s ease;
+}
+:hover {
+    z-index: ${hoverAnimation ? '1' : 'auto'};
+}
+`;
