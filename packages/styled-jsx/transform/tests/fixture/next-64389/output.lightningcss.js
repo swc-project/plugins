5d1964c5afd0b6b413dd2ed76e12079d54c9eec2
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-15 10:35:57 +0900

    Update test refs

---
+++ packages/styled-jsx/transform/tests/fixture/next-64389/output.lightningcss.js
@@ -0,0 +1,14 @@
+"use client";
+import _JSXStyle from "styled-jsx/style";
+const color = "color: red;";
+export default function RootLayout({ children }) {
+    return <html className={"jsx-22f408b5da0dc21e"}>
+
+            <head className={"jsx-22f408b5da0dc21e"}/>
+
+            <body className={"jsx-22f408b5da0dc21e"}>{children}</body>
+
+            <_JSXStyle id={"22f408b5da0dc21e"}>{`body{${color}}body p{font-size:72px}`}</_JSXStyle>
+
+        </html>;
+}
