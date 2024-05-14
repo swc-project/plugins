75ac1691adcf984f015c8bbaa111fecc7efbeaca
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-14 17:20:46 +0900

    Update test refs

---
+++ packages/loadable-components/tests/fixture/simple import/with webpackChunkName comment/should use it even if quote is single/output.js
@@ -0,0 +1,38 @@
+loadable({
+    resolved: {},
+    chunkName () {
+        return "'ChunkA'";
+    },
+    isReady (props) {
+        const key = this.resolve(props);
+        if (this.resolved[key] !== true) {
+            return false;
+        }
+        if (typeof __webpack_modules__ !== 'undefined') {
+            return !!__webpack_modules__[key];
+        }
+        return false;
+    },
+    importAsync: ()=>import(/*webpackChunkName: "'ChunkA'"*/ ""),
+    requireAsync (props) {
+        const key = this.resolve(props);
+        this.resolved[key] = false;
+        return this.importAsync(props).then((resolved)=>{
+            this.resolved[key] = true;
+            return resolved;
+        });
+    },
+    requireSync (props) {
+        const id = this.resolve(props);
+        if (typeof __webpack_require__ !== 'undefined') {
+            return __webpack_require__(id);
+        }
+        return eval('module.require')(id);
+    },
+    resolve () {
+        if (require.resolveWeak) {
+            return require.resolveWeak("");
+        }
+        return eval('require.resolve')("");
+    }
+});
