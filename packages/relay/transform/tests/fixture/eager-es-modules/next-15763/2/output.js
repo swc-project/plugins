c93e64af6fb5e9def6628c73274cd91abc929379
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-20 12:09:11 +0900

    Update test refs

---
+++ packages/relay/transform/tests/fixture/eager-es-modules/next-15763/2/output.js
@@ -0,0 +1,35 @@
+"use server";
+import __actionsSetThingMutation from "./__generated__/actionsSetThingMutation.graphql.ts";
+import { graphql } from "relay-runtime";
+import { revalidatePath } from "next/cache";
+export async function setValue(string) {
+    // comment out the next two statements to successfully compile
+    const taggedNode = __actionsSetThingMutation;
+    console.log({
+        taggedNode
+    });
+    const response = await fetch("http://localhost:3000/graphql", {
+        body: JSON.stringify({
+            // replace this query with the taggedNode value
+            query: `
+        mutation actionsSetThingMutation($value: String!) {
+          setThing(value: $value) {
+            name
+          }
+        }
+      `,
+            variables: {
+                value: string
+            }
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "POST"
+    });
+    const data = await response.json();
+    // the ui should flush this value when revalidated
+    console.log(data);
+    revalidatePath("/", 'page');
+}
+;
