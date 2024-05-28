a8abf389e6330215721eea12ee6d83adbb2ef1ca
Will Binns-Smith <wbinnssmith@gmail.com>
2024-05-28 13:21:51 -0700

    fixup! styled_jsx: update lightningcss to 1.0.0-alpha.57

--- packages/styled-jsx/transform/tests/fixture/next-55679/output.lightningcss.js
+++ packages/styled-jsx/transform/tests/fixture/next-55679/output.lightningcss.js
@@ -4,7 +4,7 @@ const someVar = "--var-1";
 export default function App({ Component, pageProps }) {
     return <>
 
-      <_JSXStyle id={"f559f0d916e6258b"}>{`:root{background-color:var(${someVar});${someVar}:red}`}</_JSXStyle>
+      <_JSXStyle id={"f559f0d916e6258b"}>{`:root{${someVar}:red;background-color:var(${someVar})}`}</_JSXStyle>
 
       <Component {...pageProps} className={"jsx-f559f0d916e6258b" + " " + (pageProps && pageProps.className != null && pageProps.className || "")}/>
 
