5e55403b180bc990f1ce8a72fd2d5d1910a5a736
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-18 10:35:04 +0900

    Update test refs

--- packages/styled-jsx/transform/tests/fixture/pack-2714/output.swc.js
+++ packages/styled-jsx/transform/tests/fixture/pack-2714/output.swc.js
@@ -1,5 +1,5 @@
 import _JSXStyle from "styled-jsx/style";
 const { className: cardClassName, styles } = {
-    styles: <_JSXStyle id={"a1e91057ca36c79d"}>{`.jsx-a1e91057ca36c79d:hover{z-index:${hoverAnimation ? "1" : "auto"}}`}</_JSXStyle>,
-    className: "jsx-a1e91057ca36c79d"
+    styles: <_JSXStyle id={"d662e9d5783da781"}>{`.jsx-d662e9d5783da781:hover{z-index:${hoverAnimation ? "1" : "auto"}}`}</_JSXStyle>,
+    className: "jsx-d662e9d5783da781"
 };
