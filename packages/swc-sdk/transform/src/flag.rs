f00c4c1c8d8957c560cdf3ca3d046d3ff7426ee6
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-09-19 15:49:34 +0900

    doc

--- packages/swc-sdk/transform/src/flag.rs
+++ packages/swc-sdk/transform/src/flag.rs
@@ -7,6 +7,70 @@ impl<C> SwcSdkTransform<C>
 where
     C: Comments,
 {
+    ///
+    /// ## Cases
+    ///
+    /// ### Empty arugments
+    ///
+    /// ```js
+    /// 
+    /// import { flag } from "@swc/sdk/flag";
+    ///
+    /// const foo = flag();
+    /// ```
+    ///
+    /// becomes
+    ///
+    /// ```js
+    /// import { flag } from "@swc/sdk/flag";
+    ///
+    /// const foo = flag({
+    ///   key: "foo",
+    /// });
+    /// ```
+    ///
+    /// ### With arguments
+    ///
+    /// ```js
+    /// import { flag } from "@swc/sdk/flag";
+    ///
+    /// const foo = flag({
+    ///     decide: () => false,
+    /// });
+    /// ```
+    ///
+    /// becomes
+    ///
+    /// ```js
+    /// import { flag } from "@swc/sdk/flag";
+    ///
+    /// const foo = flag({
+    ///     key: "foo",
+    ///     decide: () => false,
+    /// });
+    /// ```
+    ///
+    /// ### With custom adapter
+    ///
+    ///
+    /// ```js
+    /// import { flag } from "@swc/sdk/flag";
+    ///
+    /// const foo = flag(someAdapter({
+    ///     decide: () => false,
+    /// });
+    /// ```
+    ///
+    /// becomes
+    ///
+    /// ```js
+    /// import { flag } from "@swc/sdk/flag";
+    ///
+    /// const foo = flag(someAdapter({
+    ///     key: "foo",
+    ///     decide: () => false,
+    /// }));
+    /// ```
     pub(super) fn transform_flag(&mut self, v: &mut VarDeclarator) {
         let is_flag_call = v.init.as_deref().map_or(false, |e| {
             self.imports
