db3225c6f5b63465de56e266e9468766b359f996
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-09-19 17:03:41 +0900

    feat(sdk): Initialize project (#346)

---
+++ packages/swc-sdk/transform/tests/fixture.rs
@@ -0,0 +1,31 @@
+use std::path::PathBuf;
+
+use swc_common::{chain, Mark};
+use swc_ecma_parser::Syntax;
+use swc_ecma_transforms_base::resolver;
+use swc_ecma_transforms_testing::test_fixture;
+use swc_ecma_visit::as_folder;
+
+#[testing::fixture("tests/fixture/**/input.js")]
+fn pure(input: PathBuf) {
+    let output = input.parent().unwrap().join("output.js");
+    test_fixture(
+        Syntax::default(),
+        &|tr| {
+            let unresolved_mark = Mark::new();
+            let top_level_mark = Mark::new();
+
+            chain!(
+                resolver(unresolved_mark, top_level_mark, false),
+                as_folder(swc_sdk::swc_sdk(
+                    swc_sdk::Env { unresolved_mark },
+                    swc_sdk::config::Config::default(),
+                    tr.comments.clone()
+                ))
+            )
+        },
+        &input,
+        &output,
+        Default::default(),
+    );
+}
