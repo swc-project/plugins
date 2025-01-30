81ce99b3f3a6ebf0ef4228304c0e2bffb4c6280e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-31 08:53:22 +0900

    type work

--- packages/experimetal-babel/transform/src/lib.rs
+++ packages/experimetal-babel/transform/src/lib.rs
@@ -1,1 +1,22 @@
+use serde::{Deserialize, Serialize};
+use swc_ecma_ast::Program;
 
+pub struct Config {}
+
+pub struct Transform<'a> {
+    pub transform_code: &'a str,
+    pub config: &'a Config,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TransformOutput {
+    pub code: String,
+    #[serde(default)]
+    pub map: Option<String>,
+}
+
+impl Transform<'_> {
+    pub fn apply(&self, program: &Program) -> Program {
+        todo!()
+    }
+}
