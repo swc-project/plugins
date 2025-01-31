14ea5ec3fce41be0fbd61bc17f275daffcca3a14
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-01 00:03:37 +0900

    RC

---
+++ packages/experimetal-babel-react-compiler/CHANGELOG.md
@@ -0,0 +1,13 @@
+# @swc/plugin-experimental-babel
+
+## 1.0.2
+
+### Patch Changes
+
+- 9a342eb: Verify
+
+## 1.0.1
+
+### Patch Changes
+
+- 4061aa2: Initialize experiment
---
+++ packages/experimetal-babel-react-compiler/src/index.ts
@@ -0,0 +1,13 @@
+import { transformSync as innerTransform } from "@babel/core";
+import reactCompilerPlugin from "babel-plugin-react-compiler";
+
+const reactCompilerConfig = {
+  target: "19",
+};
+
+export function transform({ code, map }: { code: string; map?: string }) {
+  return innerTransform(code, {
+    plugins: [[reactCompilerPlugin, reactCompilerConfig]],
+    map,
+  });
+}
