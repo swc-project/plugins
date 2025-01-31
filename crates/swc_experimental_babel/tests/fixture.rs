2b4f158d5e214766b97b299a632246fc0bf2fce8
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-01 04:30:04 +0900

    test

--- crates/swc_experimental_babel/tests/fixture.rs
+++ crates/swc_experimental_babel/tests/fixture.rs
@@ -5,8 +5,7 @@ use swc_ecma_transforms_base::resolver;
 use swc_ecma_transforms_testing::test_fixture;
 
 #[testing::fixture("tests/basic/**/input.js")]
-
-fn fixture(input: PathBuf) {
+fn fixture_basic(input: PathBuf) {
     let output = input
         .parent()
         .expect("should have parent directory")
@@ -19,6 +18,36 @@ fn fixture(input: PathBuf) {
                 resolver(Mark::new(), Mark::new(), false),
                 swc_experimental_babel::Transform {
                     transform_code: include_str!("./basic-transform.js"),
+                    filename: Lrc::new(FileName::Real(input.clone())),
+                    cm: tr.cm.clone(),
+                },
+            )
+        },
+        &input,
+        &output,
+        Default::default(),
+    );
+}
+
+#[testing::fixture("tests/react-compiler/**/input.js")]
+fn fixture_react_compiler(input: PathBuf) {
+    let output = input
+        .parent()
+        .expect("should have parent directory")
+        .join("output.js");
+
+    test_fixture(
+        swc_ecma_parser::Syntax::Es(swc_ecma_parser::EsSyntax {
+            jsx: true,
+            ..Default::default()
+        }),
+        &|tr| {
+            (
+                resolver(Mark::new(), Mark::new(), false),
+                swc_experimental_babel::Transform {
+                    transform_code: include_str!(
+                        "../../../packages/experimetal-babel-react-compiler/dist/main.mjs"
+                    ),
                     filename: Lrc::new(FileName::Real(input.clone())),
                     cm: tr.cm.clone(),
                 },
