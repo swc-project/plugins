43c17ba00bdc6dd62311649d150804114c9cb6f4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-31 23:47:35 +0900

    package

--- packages/experimetal-babel/src/lib.rs
+++ packages/experimetal-babel/src/lib.rs
@@ -1,8 +1,39 @@
 #![allow(clippy::not_unsafe_ptr_arg_deref)]
 
+use std::sync::Arc;
+
+use serde::Deserialize;
+use swc_common::FileName;
 use swc_core::{
     ecma::ast::Program,
-    plugin::{plugin_transform, proxies::TransformPluginProgramMetadata},
+    plugin::{
+        metadata::TransformPluginMetadataContextKind, plugin_transform,
+        proxies::TransformPluginProgramMetadata,
+    },
 };
+
+#[derive(Deserialize)]
+struct Config {
+    pub transform_code: String,
+}
+
 #[plugin_transform]
-fn swc_plugin(program: Program, data: TransformPluginProgramMetadata) -> Program {}
+fn swc_plugin(program: Program, data: TransformPluginProgramMetadata) -> Program {
+    let config = serde_json::from_str::<Config>(
+        &data
+            .get_transform_plugin_config()
+            .expect("failed to get plugin config for experimental-babel"),
+    )
+    .expect("invalid config for experimental-babel");
+
+    let file_name = match data.get_context(&TransformPluginMetadataContextKind::Filename) {
+        Some(v) => FileName::Real(v.into()),
+        None => FileName::Anon,
+    };
+
+    program.apply(swc_experimental_babel::Transform {
+        transform_code: config.transform_code.as_str(),
+        cm: Arc::new(data.source_map),
+        filename: Arc::new(file_name),
+    })
+}
