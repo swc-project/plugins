2996ff743d2ec097614a25804cdd8f2a697b5b4e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-01 00:07:17 +0900

    polyfill node

--- packages/experimetal-babel-react-compiler/rsbuild.config.ts
+++ packages/experimetal-babel-react-compiler/rsbuild.config.ts
@@ -1,3 +1,6 @@
 import { defineConfig } from "@rsbuild/core";
+import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill";
 
-export default defineConfig({});
+export default defineConfig({
+  plugins: [pluginNodePolyfill()],
+});
