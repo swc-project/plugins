62a7bac466aa658ff6dee3c28f3f6dd45a7ba941
valadaptive <79560998+valadaptive@users.noreply.github.com>
2025-03-12 05:05:05 -0400

    fix(styled-components): Visit member expressions for display name (#421)
    Resolves #420. 
    
    This matches how babel-plugin-styled-components does it. (see
    [getName.js](https://github.com/styled-components/babel-plugin-styled-components/blob/4e2eb388d9c90f2921c306c760657d059d01a518/src/utils/getName.js#L39-L42)).

---
+++ packages/styled-components/transform/tests/fixtures/add-display-names-with-member-expressions/code.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const App = {};
+
+App.Foo = styled.div``;
+
+App.Foo.Bar = styled.div``
---
+++ packages/styled-components/transform/tests/fixtures/add-display-names-with-member-expressions/output.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+const App = {};
+App.Foo = styled.div.withConfig({
+    displayName: "code__Foo",
+    componentId: "sc-11752c5e-0"
+})([
+    ``
+]);
+App.Foo.Bar = styled.div.withConfig({
+    displayName: "code__Bar",
+    componentId: "sc-11752c5e-1"
+})([
+    ``
+]);
