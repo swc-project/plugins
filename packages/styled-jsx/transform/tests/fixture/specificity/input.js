6799f685f15a6730dada86c7a122b1e346411c89
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-22 09:40:22 -0700

    Add a test

---
+++ packages/styled-jsx/transform/tests/fixture/specificity/input.js
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <>
+            <style jsx>{`
+            div {
+                &.parent {
+                color: green;
+                }
+            }
+        `}</style>
+        </>
+    );
+}
