89180683b66275cf6cc786a86c8e5b8101679a95
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-28 09:59:10 -0700

    Update test refs

--- packages/styled-jsx/transform/tests/fixture/next-65064/1/output.lightningcss.js
+++ packages/styled-jsx/transform/tests/fixture/next-65064/1/output.lightningcss.js
@@ -6,7 +6,7 @@ export default function SimplePage() {
     return <ComponentWithChildAsProp trigger={<>
                     <div className={"jsx-68234eda9c798fae" + " " + "text animated"}>Text</div>
 
-                    <_JSXStyle id={"68234eda9c798fae"}>{".text.jsx-68234eda9c798fae{color:#00f}.text.jsx-68234eda9c798fae.jsx-68234eda9c798fae:hover{color:red}"}</_JSXStyle>
+                    <_JSXStyle id={"68234eda9c798fae"}>{".text.jsx-68234eda9c798fae{color:#00f}.text.jsx-68234eda9c798fae:hover{color:red}"}</_JSXStyle>
                 </>}/>;
 }
 const ComponentWithChildAsProp = ({ trigger })=>{
