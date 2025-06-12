f9213d33d0fa6331c1ca993253030739480ee2e6
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-06-12 09:46:19 -0700

    fix(styled-jsx): Fix content escaping regression (#471)

--- packages/styled-jsx/transform/tests/fixture/tpl-escape-2/output.lightningcss.js
+++ packages/styled-jsx/transform/tests/fixture/tpl-escape-2/output.lightningcss.js
@@ -10,6 +10,6 @@ export default function Home({ fontFamily }) {
     ])}>
       <_JSXStyle id={"b45f7ae33801eec9"} dynamic={[
         fontFamily
-    ]}>{`body{font-family:${fontFamily}}code:before,code:after{content:\"\`\"}`}</_JSXStyle>
+    ]}>{`body{font-family:${fontFamily}}code:before,code:after{content:"\`"}`}</_JSXStyle>
     </div>;
 }
