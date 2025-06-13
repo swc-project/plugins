2d46106ff757a5c2f3ee880b79bd45833180868b
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-06-13 13:15:02 -0700

    Update test refs

---
+++ packages/styled-jsx/transform/tests/fixture/global-nested/output.lightningcss.js
@@ -0,0 +1,5 @@
+import _JSXStyle from "styled-jsx/style";
+const Test = ()=><div className={"jsx-258c8cca0b71d485"}>
+        <span className={"jsx-258c8cca0b71d485"}>test</span>
+        <_JSXStyle id={"258c8cca0b71d485"}>{".p1.jsx-258c8cca0b71d485{background:purple!important}.p1.jsx-258c8cca0b71d485 .c1{background:orange!important}.p1.jsx-258c8cca0b71d485 .c2.jsx-258c8cca0b71d485{background:orange!important}"}</_JSXStyle>
+    </div>;
