1b05f5ac6e062e6cf2707bd4f2b2f9d7551be437
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-06-24 16:58:53 +0900

    fix(styled-jsx): Fix `&` mixed with `:global` correctly (#480)

---
+++ packages/styled-jsx/transform/tests/fixture/global-nested-scope-3/input.js
@@ -0,0 +1,14 @@
+const Test = () => (
+  <div>
+    <span>test</span>
+    <style jsx>{`
+        .parent {
+          :global(div) {
+            :global(&.child > .child2) {
+              background: orange;
+            }
+          }
+        }
+      `}</style>
+  </div>
+);
