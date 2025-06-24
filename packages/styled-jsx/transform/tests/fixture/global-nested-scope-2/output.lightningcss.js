3175e4ace9467a96113d9a169fa33f83ff260d37
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-06-24 16:52:55 +0900

    Update test refs

---
+++ packages/styled-jsx/transform/tests/fixture/global-nested-scope-2/output.lightningcss.js
@@ -0,0 +1,5 @@
+import _JSXStyle from "styled-jsx/style";
+const Test = ()=><div className={"jsx-9303248a3896bf50"}>
+    <span className={"jsx-9303248a3896bf50"}>test</span>
+    <_JSXStyle id={"9303248a3896bf50"}>{".parent.jsx-9303248a3896bf50 div :is(.child,.child2){background:orange}"}</_JSXStyle>
+  </div>;
