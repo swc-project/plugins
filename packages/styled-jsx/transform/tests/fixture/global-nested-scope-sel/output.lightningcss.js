9cc986f36162cf44b806e397e67fe6a32ccc8362
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-06-24 13:44:42 +0900

    Test snapshot

--- packages/styled-jsx/transform/tests/fixture/global-nested-scope-sel/output.lightningcss.js
+++ packages/styled-jsx/transform/tests/fixture/global-nested-scope-sel/output.lightningcss.js
@@ -1,5 +1,5 @@
 import _JSXStyle from "styled-jsx/style";
 const Test = ()=><div className={"jsx-b0a16d58836c444b"}>
     <span className={"jsx-b0a16d58836c444b"}>test</span>
-    <_JSXStyle id={"b0a16d58836c444b"}>{".parent.jsx-b0a16d58836c444b{}.parent.jsx-b0a16d58836c444b div{}.parent.jsx-b0a16d58836c444b div.child{background:orange}"}</_JSXStyle>
+    <_JSXStyle id={"b0a16d58836c444b"}>{".parent.jsx-b0a16d58836c444b div.child{background:orange}"}</_JSXStyle>
   </div>;
