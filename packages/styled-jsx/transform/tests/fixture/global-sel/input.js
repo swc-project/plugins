501f103c441dd378367c38ce41058da92824becb
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-07-01 08:08:27 +0900

    Add a test

---
+++ packages/styled-jsx/transform/tests/fixture/global-sel/input.js
@@ -0,0 +1,11 @@
+const Test = () => (
+  <div>
+    <span>test</span>
+    <style jsx>{`
+        :global(.c1),
+        .c2 {
+          background: orange !important;
+        }
+      `}</style>
+  </div>
+);
