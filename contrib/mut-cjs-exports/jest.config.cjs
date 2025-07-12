2bf57e2d2ccfadce0d3dc42e63fd2cad84dc17a1
magic-akari <akari.ccino@gmail.com>
2025-07-12 10:55:10 +0800

    fix(build): fixup config

--- contrib/mut-cjs-exports/jest.config.cjs
+++ contrib/mut-cjs-exports/jest.config.cjs
@@ -1,14 +1,6 @@
 const path = require("node:path");
 
-const isDev = process.env.WASM_ENV !== "release";
-
-const plugin = isDev
-  ? path.resolve(
-      "./target/wasm32-unknown-unknown/debug/swc_mut_cjs_exports.wasm",
-    )
-  : path.resolve(
-      "./target/wasm32-unknown-unknown/release/swc_mut_cjs_exports.wasm",
-    );
+const plugin = path.resolve("./swc_mut_cjs_exports_debug.wasm");
 
 module.exports = {
   transform: {
