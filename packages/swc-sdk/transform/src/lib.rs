2d7ba0f96cc12eab26475d3677382c756329ee5d
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-11-10 18:56:38 +0900

    jsx

--- packages/swc-sdk/transform/src/lib.rs
+++ packages/swc-sdk/transform/src/lib.rs
@@ -3,8 +3,9 @@
 
 use swc_common::{comments::Comments, errors::HANDLER, util::take::Take, Mark, DUMMY_SP};
 use swc_ecma_ast::{
-    ArrowExpr, AwaitExpr, BlockStmtOrExpr, CallExpr, Callee, Expr, Function, Ident, IdentName,
-    Import, MemberExpr, MemberProp, Module, ModuleDecl, ModuleItem, VarDeclarator,
+    ArrowExpr, AwaitExpr, BlockStmtOrExpr, CallExpr, Callee, Decl, Expr, Function, Ident,
+    IdentName, Import, JSXElementName, MemberExpr, MemberProp, Module, ModuleDecl, ModuleItem,
+    Stmt, VarDecl, VarDeclKind, VarDeclarator,
 };
 use swc_ecma_utils::{private_ident, ExprFactory};
 use swc_ecma_visit::{VisitMut, VisitMutWith};
@@ -30,6 +31,8 @@ where
         comments,
         imports: Default::default(),
         can_use_await: Default::default(),
+        jsx_lazy_import: Default::default(),
+        vars: Default::default(),
     }
 }
 
@@ -45,7 +48,11 @@ where
     comments: C,
     imports: ImportMap,
 
+    jsx_lazy_import: Option<Ident>,
+
     can_use_await: bool,
+
+    vars: Vec<VarDeclarator>,
 }
 
 impl<C> SwcSdkTransform<C>
@@ -105,7 +112,77 @@ where
     }
 
     fn visit_mut_expr(&mut self, node: &mut Expr) {
-        if let Expr::Call(call) = &mut *node {
+        if let Expr::JSXElement(jsx) = &mut *node {
+            if let JSXElementName::Ident(ident) = &jsx.opening.name {
+                let node = Expr::Ident(ident.clone());
+
+                if let Some((import_span, source, export_name)) =
+                    self.imports.should_make_dynamic(&node)
+                {
+                    let module_param = private_ident!("module");
+
+                    let import_call = CallExpr {
+                        span: import_span,
+                        callee: Callee::Import(Import {
+                            span: import_span,
+                            phase: Default::default(),
+                        }),
+                        args: vec![source.clone().as_arg()],
+                        ..Default::default()
+                    }
+                    .into();
+
+                    let then_arg = Expr::Arrow(ArrowExpr {
+                        span: DUMMY_SP,
+                        params: vec![module_param.clone().into()],
+                        body: BlockStmtOrExpr::Expr(Box::new(Expr::Member(MemberExpr {
+                            span: DUMMY_SP,
+                            obj: module_param.clone().into(),
+                            prop: MemberProp::Ident(IdentName::new(export_name.clone(), DUMMY_SP)),
+                        })))
+                        .into(),
+                        is_async: true,
+                        is_generator: false,
+                        ..Default::default()
+                    });
+
+                    let then_expr = Expr::Call(CallExpr {
+                        span: DUMMY_SP,
+                        callee: MemberExpr {
+                            span: DUMMY_SP,
+                            obj: import_call,
+                            prop: MemberProp::Ident("then".into()),
+                        }
+                        .as_callee(),
+                        args: vec![then_arg.as_arg()],
+                        ..Default::default()
+                    });
+
+                    let lazy_import = self
+                        .jsx_lazy_import
+                        .get_or_insert_with(|| private_ident!("lazy"));
+
+                    let lazy_component = CallExpr {
+                        span: DUMMY_SP,
+                        callee: lazy_import.clone().as_callee(),
+                        args: vec![then_expr.as_arg()],
+                        ..Default::default()
+                    };
+
+                    let lazy_var_name = private_ident!("LazyComponent");
+
+                    self.vars.push(VarDeclarator {
+                        span: DUMMY_SP,
+                        name: lazy_var_name.clone().into(),
+                        init: Some(lazy_component.into()),
+                        definite: false,
+                    });
+
+                    jsx.opening.name = JSXElementName::Ident(lazy_var_name.clone());
+                    return;
+                }
+            }
+        } else if let Expr::Call(call) = &mut *node {
             if let Callee::Expr(callee) = &mut call.callee {
                 if let Some((import_span, source, export_name)) =
                     self.imports.should_make_dynamic(callee)
@@ -199,6 +276,54 @@ where
         }
 
         m.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_module_items(&mut self, stmts: &mut Vec<ModuleItem>) {
+        let mut new_stmts = Vec::with_capacity(stmts.len());
+        let old_vars = self.vars.take();
+
+        for mut stmt in stmts.take() {
+            stmt.visit_mut_with(self);
+
+            if !self.vars.is_empty() {
+                stmt = ModuleItem::Stmt(Stmt::Decl(Decl::Var(Box::new(VarDecl {
+                    span: DUMMY_SP,
+                    ctxt: Default::default(),
+                    kind: VarDeclKind::Const,
+                    declare: false,
+                    decls: self.vars.take(),
+                }))));
+            }
+
+            new_stmts.push(stmt);
+        }
+
+        *stmts = new_stmts;
+        self.vars = old_vars;
+    }
+
+    fn visit_mut_stmts(&mut self, stmts: &mut Vec<Stmt>) {
+        let mut new_stmts = Vec::with_capacity(stmts.len());
+        let old_vars = self.vars.take();
+
+        for mut stmt in stmts.take() {
+            stmt.visit_mut_with(self);
+
+            if !self.vars.is_empty() {
+                stmts.push(Stmt::Decl(Decl::Var(Box::new(VarDecl {
+                    span: DUMMY_SP,
+                    ctxt: Default::default(),
+                    kind: VarDeclKind::Const,
+                    declare: false,
+                    decls: self.vars.take(),
+                }))));
+            }
+
+            new_stmts.push(stmt);
+        }
+
+        *stmts = new_stmts;
+        self.vars = old_vars;
     }
 
     fn visit_mut_var_declarator(&mut self, node: &mut VarDeclarator) {
