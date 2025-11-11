08d00cc3f7e27c2171092605afd658f0c0a45a32
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-11-11 13:50:08 +0900

    Update test refs

--- packages/swc-sdk/transform/tests/fixture/dynamic-imports/jsx-sync/output.js
+++ packages/swc-sdk/transform/tests/fixture/dynamic-imports/jsx-sync/output.js
@@ -1,7 +1,8 @@
 /*#__DYNAMIC__*/ import { lazy as lazy } from "react";
+let LazyComponent;
 export function App() {
     if (process.env.NODE_ENV !== "production") {
-        const LazyComponent = lazy(()=>import("./stripped").then(async (module)=>module.Component));
+        LazyComponent ??= lazy(()=>import("./stripped").then(async (module)=>module.Component));
         return <LazyComponent/>;
     }
     return <></>;
