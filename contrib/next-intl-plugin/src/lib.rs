e3c4205270a37545bdac793c042b802ffb0cccbb
DongYun Kang <kdy.1997.dev@gmail.com>
2025-11-14 11:55:28 +0900

    TransformVisitor

--- contrib/next-intl-plugin/src/lib.rs
+++ contrib/next-intl-plugin/src/lib.rs
@@ -2,12 +2,31 @@
 #![feature(box_patterns)]
 
 use swc_core::plugin::proxies::TransformPluginProgramMetadata;
-use swc_ecma_ast::Program;
+use swc_ecma_ast::{Module, Program};
+use swc_ecma_visit::{VisitMut, VisitMutWith};
 use swc_plugin_macro::plugin_transform;
+
+use crate::import_map::ImportMap;
 
 mod import_map;
 
 #[plugin_transform]
-fn plugin(program: Program, _: TransformPluginProgramMetadata) -> Program {
+fn next_intl_plugin(mut program: Program, _: TransformPluginProgramMetadata) -> Program {
+    program.visit_mut_with(&mut TransformVisitor {
+        imports: ImportMap::default(),
+    });
+
     program
+}
+
+struct TransformVisitor {
+    imports: ImportMap,
+}
+
+impl VisitMut for TransformVisitor {
+    fn visit_mut_module(&mut self, module: &mut Module) {
+        self.imports = ImportMap::analyze(module);
+
+        module.visit_mut_children_with(self);
+    }
 }
