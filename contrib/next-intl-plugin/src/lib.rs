ff59f1e38b96e36c46c1cb4e738fcf68dd50b790
DongYun Kang <kdy.1997.dev@gmail.com>
2025-11-14 11:53:57 +0900

    import_map

--- contrib/next-intl-plugin/src/lib.rs
+++ contrib/next-intl-plugin/src/lib.rs
@@ -1,8 +1,11 @@
 #![allow(clippy::not_unsafe_ptr_arg_deref)]
+#![feature(box_patterns)]
 
 use swc_core::plugin::proxies::TransformPluginProgramMetadata;
 use swc_ecma_ast::Program;
 use swc_plugin_macro::plugin_transform;
+
+mod import_map;
 
 #[plugin_transform]
 fn plugin(program: Program, _: TransformPluginProgramMetadata) -> Program {
