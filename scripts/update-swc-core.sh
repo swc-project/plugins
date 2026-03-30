15a387eba0abb7b9224b5027e6ba63f6a566934e
DongYun Kang <kdy.1997.dev@gmail.com>
2026-03-30 21:58:58 +0900

    build: Update swc_core

--- scripts/update-swc-core.sh
+++ scripts/update-swc-core.sh
@@ -3,10 +3,10 @@ set -eu
 
 curl https://raw.githubusercontent.com/swc-project/swc/main/scripts/update-all-swc-crates.sh | bash -s
 
-git add -A && git commit -m "build: Update swc core"
+git add -A && git commit -m "build: Update swc_core"
 
 echo "Bumping crates"
-./scripts/bump-crates.sh --breaking
+./scripts/bump-crates.sh --level major
 
 echo "Bumping npm packages"
 ./scripts/bump-npm.sh
